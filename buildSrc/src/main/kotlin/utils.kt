96b09553a09c2ae8f83ce1a639b7aa3b7d807e61
David Denton <denton.david@gmail.com>
2020-09-15 12:12:57 +0100

    use gradle property for version

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -3,12 +3,13 @@ import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.kotlin
 
 fun Project.kotlinProject() {
+
     dependencies {
         "implementation"(kotlin("stdlib"))
-        "implementation"(platform("org.http4k:http4k-bom:3.260.0"))
+        "implementation"(platform("org.http4k:http4k-bom:${properties["http4k_version"]!!}"))
         "implementation"("org.http4k:http4k-core")
 
-        "testImplementation"(platform("org.junit:junit-bom:5.7.0"))
+        "testImplementation"(platform("org.junit:junit-bom:${properties["junit_version"]!!}"))
         "testImplementation"("org.http4k:http4k-testing-hamkrest")
         "testImplementation"("org.junit.jupiter:junit-jupiter-api")
         "testImplementation"("org.junit.jupiter:junit-jupiter-engine")
