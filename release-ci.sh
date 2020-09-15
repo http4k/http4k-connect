2ffc423da576181784856c487cedc042e2cc2bc3
David Denton <denton.david@gmail.com>
2020-09-15 13:47:37 +0100

    release to bintray

---
+++ release-ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -o errexit
+set -o pipefail
+set -o nounset
+
+NEW_VERSION=$1
+
+echo "Attempting to release $NEW_VERSION"
+
+./gradlew -PreleaseVersion="$NEW_VERSION" clean test \
+    http4k-connect-bom:bintrayUpload \
+    http4k-connect-example:bintrayUpload \
+    http4k-connect-example-fake:bintrayUpload \
