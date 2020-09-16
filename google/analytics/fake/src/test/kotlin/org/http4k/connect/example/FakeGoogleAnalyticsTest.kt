606cadb04e83153f5495e37b13b1d8ad579c976b
David Denton <denton.david@gmail.com>
2020-09-16 16:39:13 +0100

    add system contract

--- google/analytics/fake/src/test/kotlin/org/http4k/connect/example/FakeGoogleAnalyticsTest.kt
+++ google/analytics/fake/src/test/kotlin/org/http4k/connect/example/FakeGoogleAnalyticsTest.kt
@@ -2,18 +2,15 @@ package org.http4k.connect.example
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.connect.common.failure
 import org.http4k.connect.common.success
 import org.http4k.connect.google.FakeGoogleAnalytics
 import org.http4k.connect.google.analytics.ClientId
 import org.http4k.connect.google.analytics.GoogleAnalytics
 import org.http4k.connect.google.analytics.Http
-import org.http4k.connect.google.analytics.RemoteFailure
 import org.http4k.connect.google.analytics.TrackingId
-import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
 import org.junit.jupiter.api.Test
 
-class FakeGoogleAnalyticsTest {
+class FakeGoogleAnalyticsTest : FakeSystemContract() {
     private val http = FakeGoogleAnalytics()
     private val analytics = GoogleAnalytics.Http(http, TrackingId("SOME_TRACKING_ID"))
 
@@ -23,13 +20,4 @@ class FakeGoogleAnalyticsTest {
             analytics.pageView("some-user-agent", ClientId("SOME_CLIENT_ID"), "title", "/doc/path", "www.http4k.org"),
             equalTo(success(Unit)))
     }
-
-    @Test
-    fun `on error`() {
-        http.returnStatus(INTERNAL_SERVER_ERROR)
-        assertThat(
-            analytics.pageView("some-user-agent", ClientId("SOME_CLIENT_ID"), "title", "/doc/path", "www.http4k.org"),
-            equalTo(failure(RemoteFailure(INTERNAL_SERVER_ERROR))))
-    }
-}
 
