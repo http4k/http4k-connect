e62a5341000d4e26002f1abe2a1a47f8978365df
David Denton <denton.david@gmail.com>
2020-09-17 16:45:32 +0100

    common module reused

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,21 @@
-include("common")
+includeWithName("http4k-connect-common", "common")
 include("http4k-connect-bom")
 includeSystem("example")
 includeSystem("google", "analytics")
 
 fun includeSystem(system: String) {
     val projectName = "http4k-connect-$system"
-    include(
-        ":$projectName",
-        ":$projectName-fake"
-    )
-    project(":$projectName").projectDir = File("$system/client")
-    project(":$projectName-fake").projectDir = File("$system/fake")
+    includeWithName(projectName, "$system/client")
+    includeWithName("$projectName-fake", "$system/fake")
 }
 
 fun includeSystem(owner: String, system: String) {
     val projectName = "http4k-connect-$owner-$system"
-    include(
-        ":$projectName",
-        ":$projectName-fake"
-    )
-    project(":$projectName").projectDir = File("$owner/$system/client")
-    project(":$projectName-fake").projectDir = File("$owner/$system/fake")
+    includeWithName(projectName, "$owner/$system/client")
+    includeWithName("$projectName-fake", "$owner/$system/$owner/$system/client")
+}
+
+fun includeWithName(projectName: String, file: String) {
+    include(":$projectName")
+    project(":$projectName").projectDir = File(file)
 }
