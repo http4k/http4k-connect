462d1f4a75458df03b21cb6daedb39458c52e3ad
David Denton <denton.david@gmail.com>
2020-10-01 08:38:07 +0100

    add loading of env file

--- amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RealS3Test.kt
+++ amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RealS3Test.kt
@@ -1,23 +1,44 @@
 package org.http4k.connect.amazon.s3
 
+import org.http4k.aws.AwsCredentialScope
+import org.http4k.aws.AwsCredentials
 import org.http4k.client.JavaHttpClient
+import org.http4k.cloudnative.env.Environment
+import org.http4k.cloudnative.env.EnvironmentKey
+import org.http4k.cloudnative.env.fromConfigFile
 import org.http4k.core.HttpHandler
-import org.http4k.core.Uri
-import org.http4k.core.then
-import org.http4k.filter.ClientFilters
-import org.junit.jupiter.api.Disabled
+import org.http4k.lens.composite
 import org.junit.jupiter.api.Test
+import java.io.File
+import java.util.UUID
 
 interface S3Contract {
     val http: HttpHandler
+    val credentials: AwsCredentials
+    val scope: AwsCredentialScope
 
     @Test
     fun `lifecycle`() {
-
+        val bucketName = BucketName(UUID.randomUUID().toString())
+        val http1 = S3.Http(http, scope, { credentials })
     }
 }
 
-@Disabled
 class RealS3Test : S3Contract {
-    override val http = ClientFilters.SetBaseUriFrom(Uri.of("http://localhost:9876")).then(JavaHttpClient())
+    override val credentials: AwsCredentials
+    override val scope: AwsCredentialScope
+
+    init {
+        val env = Environment.fromConfigFile(File(System.getProperty("user.home"), ".aws/config")) overrides
+            Environment.fromConfigFile(File(System.getProperty("user.home"), ".aws/credentials"))
+        credentials = EnvironmentKey.composite {
+            AwsCredentials(
+                EnvironmentKey.required("default-aws-secret-access-key")(it),
+                EnvironmentKey.required("default-aws-access-key-id")(it)
+            )
+        }(env)
+        scope = AwsCredentialScope(EnvironmentKey.required("default-region")(env), "s3")
+    }
+
+    override val http = JavaHttpClient()
 }
