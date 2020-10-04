9c72b62ae8aba42df461e41b8550c0a2ebf78928
David Denton <denton.david@gmail.com>
2020-10-04 18:59:35 +0100

    tidy gradles

--- example/client/build.gradle.kts
+++ example/client/build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    kotlin("jvm")
-}
-
 dependencies {
     testImplementation(project(":http4k-connect-example-fake"))
 }
--- storage/redis/build.gradle.kts
+++ storage/redis/build.gradle.kts
@@ -1,9 +1,5 @@
-plugins {
-    kotlin("jvm")
-}
-
 dependencies {
-    "implementation"(project(":http4k-connect-core"))
-    "implementation"("io.lettuce:lettuce-core:5.3.4.RELEASE")
-    "implementation"("org.http4k:http4k-format-jackson")
+    implementation(project(":http4k-connect-core"))
+    implementation("org.http4k:http4k-format-jackson")
+    implementation("io.lettuce:lettuce-core:5.3.4.RELEASE")
 }
