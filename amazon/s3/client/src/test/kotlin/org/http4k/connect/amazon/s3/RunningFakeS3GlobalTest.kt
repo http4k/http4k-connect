9b29884c30d10b5cc3de7a9c6b7af56174b8972e
David Denton <denton.david@gmail.com>
2020-10-07 16:06:07 +0100

    upgrade http4k

--- amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RunningFakeS3GlobalTest.kt
+++ amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RunningFakeS3GlobalTest.kt
@@ -5,13 +5,11 @@ import org.http4k.connect.defaultPort
 import org.http4k.core.SetAuthorityFrom
 import org.http4k.core.Uri
 import org.http4k.core.then
-import org.http4k.filter.DebuggingFilters
 import org.http4k.server.Http4kServer
 import org.junit.jupiter.api.AfterEach
 
 class RunningFakeS3GlobalTest : S3GlobalContract(
     SetAuthorityFrom(Uri.of("http://localhost:" + FakeS3::class.defaultPort()))
-        .then(DebuggingFilters.PrintRequestAndResponse())
         .then(JavaHttpClient())
 ) {
     override val aws = fakeAwsEnvironment
