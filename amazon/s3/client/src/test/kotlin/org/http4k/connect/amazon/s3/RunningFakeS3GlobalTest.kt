e779a16eb1b35e769fef35d19c0d983c8c48116f
David Denton <denton.david@gmail.com>
2020-10-08 14:55:58 +0100

    use existing filter instead of custom one

--- amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RunningFakeS3GlobalTest.kt
+++ amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RunningFakeS3GlobalTest.kt
@@ -2,14 +2,14 @@ package org.http4k.connect.amazon.s3
 
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.defaultPort
-import org.http4k.core.SetAuthorityFrom
 import org.http4k.core.Uri
 import org.http4k.core.then
+import org.http4k.filter.ClientFilters
 import org.http4k.server.Http4kServer
 import org.junit.jupiter.api.AfterEach
 
 class RunningFakeS3GlobalTest : S3GlobalContract(
-    SetAuthorityFrom(Uri.of("http://localhost:" + FakeS3::class.defaultPort()))
+    ClientFilters.SetAuthorityFrom(Uri.of("http://localhost:" + FakeS3::class.defaultPort()))
         .then(JavaHttpClient())
 ) {
     override val aws = fakeAwsEnvironment
