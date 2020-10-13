7956099df908fae99553a9d770af3a072dc0f7dd
David Denton <denton.david@gmail.com>
2020-10-13 11:45:22 +0100

    comment

--- core/fake/src/main/kotlin/org/http4k/connect/storage/Storage.kt
+++ core/fake/src/main/kotlin/org/http4k/connect/storage/Storage.kt
@@ -4,14 +4,11 @@ package org.http4k.connect.storage
  * Storage for a set of objects keyed by String
  */
 
-// maybe generify key
 interface Storage<T : Any> {
     operator fun get(key: String): T?
     operator fun set(key: String, data: T)
     fun remove(key: String): Boolean
-
     fun keySet(keyPrefix: String): Set<String>
-
     fun removeAll(keyPrefix: String = ""): Boolean
 
     companion object
