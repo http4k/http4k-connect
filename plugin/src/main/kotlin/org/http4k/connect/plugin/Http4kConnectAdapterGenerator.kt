93b44b2a616968e2b961b72a87d3e5dd1dfeb03f
David Denton <denton.david@gmail.com>
2020-12-14 20:08:49 +0000

    working on plugin to generate extensions

--- plugin/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterGenerator.kt
+++ plugin/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterGenerator.kt
@@ -1,6 +1,9 @@
 package org.http4k.connect.plugin
 
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.metadata.ImmutableKmClass
 import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
 import com.squareup.kotlinpoet.metadata.toImmutableKmClass
 import org.http4k.connect.Http4kConnectAction
@@ -16,42 +19,55 @@ import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 import javax.tools.Diagnostic.Kind.ERROR
 
+@KotlinPoetMetadataPreview
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes("org.http4k.connect.Http4kConnectAdapter")
 @SupportedOptions(KAPT_KOTLIN_GENERATED_OPTION_NAME)
 class Http4kConnectAdapterGenerator : AbstractProcessor() {
 
-    @OptIn(KotlinPoetMetadataPreview::class)
-    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment): Boolean {
-        val annotatedElements = roundEnv.getElementsAnnotatedWith(Http4kConnectAdapter::class.java)
-        if (annotatedElements.isEmpty()) return false
-
-        val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] ?: run {
+    override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        outputDir() ?: run {
             processingEnv.messager.printMessage(ERROR, "Can't find the target directory for generated Kotlin files.")
             return false
         }
 
-        return roundEnv.annotated<Http4kConnectAdapter>()
+        roundEnv.annotated<Http4kConnectAdapter>()
             .filterIsInstance<TypeElement>()
             .firstOrNull()
             ?.toImmutableKmClass()
-            ?.let { adapter ->
-                roundEnv.annotated<Http4kConnectAction>()
-                    .filterIsInstance<TypeElement>()
-                    .map { it.toImmutableKmClass() }
-                    .forEach {
-                        println(it)
-                    }
+            ?.generateActionExtensionsFor(roundEnv)
 
-                FileSpec.builder(
-                    adapter.name.substringBeforeLast("/").replace('/', '.'),
-                    adapter.name.substringAfterLast('/').toLowerCase() + "Extensions").build()
-                    .writeTo(File(kaptKotlinGeneratedDir))
-
-                true
-            }
-            ?: false
+        return true
     }
+
+    private fun ImmutableKmClass.generateActionExtensionsFor(roundEnv: RoundEnvironment) {
+        val (packageName, className) = explodeName()
+
+
+        val fileBuilder = FileSpec.builder(
+            packageName,
+            className.toLowerCase() + "Extensions")
+
+        roundEnv.annotated<Http4kConnectAction>()
+            .filterIsInstance<TypeElement>()
+            .map { it.toImmutableKmClass() }
+            .forEach {
+                val (actionPkg, actionClazz) = it.explodeName()
+                fileBuilder.addFunction(FunSpec.builder(actionClazz.decapitalize())
+                    .receiver(ClassName.bestGuess(name.replace('/', '.')))
+                    .build())
+            }
+
+        fileBuilder.build().writeTo(File(outputDir()!!))
+    }
+
+    private fun ImmutableKmClass.explodeName(): Pair<String, String> {
+        val packageName = name.substringBeforeLast("/").replace('/', '.')
+        val className = name.substringAfterLast('/')
+        return packageName to className
+    }
+
+    private fun outputDir() = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
 
     companion object {
         const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
