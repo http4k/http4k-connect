bf1628e44f6653861ab38046df77a0770de654f3
David Denton <denton.david@gmail.com>
2021-01-02 10:13:57 +0000

    fixing up create queue calls

--- amazon/sqs/client/src/test/kotlin/org/http4k/connect/amazon/sqs/SQSContract.kt
+++ amazon/sqs/client/src/test/kotlin/org/http4k/connect/amazon/sqs/SQSContract.kt
@@ -2,12 +2,29 @@ package org.http4k.connect.amazon.sqs
 
 import org.http4k.connect.amazon.AwsContract
 import org.http4k.connect.amazon.model.AwsService
+import org.http4k.connect.amazon.model.QueueName
+import org.http4k.connect.successValue
 import org.http4k.core.HttpHandler
 import org.http4k.filter.debug
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.time.ZonedDateTime
+import java.util.UUID
 
 abstract class SQSContract(http: HttpHandler) : AwsContract(AwsService.of("sqs"), http) {
 
     private val sqs by lazy {
         SQS.Http(aws.scope, { aws.credentials }, http.debug())
+    }
+
+    private val queueName = QueueName.of(UUID.randomUUID().toString())
+    private val expires = ZonedDateTime.now().plus(Duration.ofMinutes(1))
+
+    @Test
+    fun `queue lifecycle`() {
+        sqs.createQueue(queueName,
+            mapOf("tag" to "value"),
+            mapOf("MaximumMessageSize" to "10000"),
+            expires).successValue()
     }
 }
