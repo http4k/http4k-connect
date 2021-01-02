c82e01b5443d989243392287b0158958ea4ba922
David Denton <denton.david@gmail.com>
2021-01-02 21:45:08 +0000

    disable test

--- amazon/kms/client/src/test/kotlin/org/http4k/connect/amazon/kms/RealKMSTest.kt
+++ amazon/kms/client/src/test/kotlin/org/http4k/connect/amazon/kms/RealKMSTest.kt
@@ -3,7 +3,9 @@ package org.http4k.connect.amazon.kms
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
+import org.junit.jupiter.api.Disabled
 
+@Disabled // until policy sorted out
 class RealKMSTest : KMSContract(JavaHttpClient()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
 }
