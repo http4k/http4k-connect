1c29b74f39c68702f0d8a32da3d41a0da3d24a0b
David Denton <denton.david@gmail.com>
2021-01-08 19:59:27 +0000

    S3 actions are fixed

--- amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RealS3GlobalTest.kt
+++ amazon/s3/client/src/test/kotlin/org/http4k/connect/amazon/s3/RealS3GlobalTest.kt
@@ -3,8 +3,7 @@ package org.http4k.connect.amazon.s3
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
-import org.http4k.filter.debug
 
-class RealS3GlobalTest : S3GlobalContract(JavaHttpClient().debug()), RealAwsEnvironment {
+class RealS3GlobalTest : S3GlobalContract(JavaHttpClient()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
 }
