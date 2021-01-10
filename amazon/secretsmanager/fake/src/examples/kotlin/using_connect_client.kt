a2d9ecd086991e47fb7017437be6d77f44773e1b
David Denton <denton.david@gmail.com>
2021-01-10 18:02:19 +0000

    convert readme

--- amazon/secretsmanager/fake/src/examples/kotlin/using_connect_client.kt
+++ amazon/secretsmanager/fake/src/examples/kotlin/using_connect_client.kt
@@ -1,16 +1,20 @@
-# Secrets Manager
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.valueOrNull
+import org.http4k.aws.AwsCredentials
+import org.http4k.client.JavaHttpClient
+import org.http4k.connect.RemoteFailure
+import org.http4k.connect.amazon.model.Region
+import org.http4k.connect.amazon.model.SecretId
+import org.http4k.connect.amazon.secretsmanager.FakeSecretsManager
+import org.http4k.connect.amazon.secretsmanager.Http
+import org.http4k.connect.amazon.secretsmanager.SecretsManager
+import org.http4k.connect.amazon.secretsmanager.action.CreatedSecret
+import org.http4k.connect.amazon.secretsmanager.createSecret
+import org.http4k.connect.amazon.secretsmanager.getSecretValue
+import org.http4k.core.HttpHandler
+import org.http4k.filter.debug
+import java.util.UUID
 
-The Secrets Manager connector provides the following Actions:
-
-     *  CreateSecret
-     *  DeleteSecret
-     *  GetSecretValue
-     *  ListSecrets
-     *  PutSecretValue
-     *  UpdateSecret
-
-### Example usage
-```kotlin
 const val USE_REAL_CLIENT = false
 
 fun main() {
@@ -31,12 +35,3 @@ fun main() {
     // get the secret value back
     println(client.getSecretValue(secretId).valueOrNull())
 }
-```
-The client APIs utilise the `http4k-aws` module for request signing, which means no dependencies on the incredibly fat Amazon-SDK JARs. This means this integration is perfect for running Serverless Lambdas where binary size is a performance factor.
-
-### Default Fake port: 58194
-
-To start:
-```
-FakeSecretsManager().start()
-```
