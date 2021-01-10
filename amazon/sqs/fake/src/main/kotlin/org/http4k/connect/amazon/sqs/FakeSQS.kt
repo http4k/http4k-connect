a0e67e9f282b576704a218d6beaf045501dcb8d4
David Denton <denton.david@gmail.com>
2021-01-10 18:45:19 +0000

    extract endpoints from FakeSQS

--- amazon/sqs/fake/src/main/kotlin/org/http4k/connect/amazon/sqs/FakeSQS.kt
+++ amazon/sqs/fake/src/main/kotlin/org/http4k/connect/amazon/sqs/FakeSQS.kt
@@ -3,107 +3,37 @@ package org.http4k.connect.amazon.sqs
 import org.http4k.aws.AwsCredentials
 import org.http4k.connect.ChaosFake
 import org.http4k.connect.amazon.model.AwsAccount
-import org.http4k.connect.amazon.model.ReceiptHandle
 import org.http4k.connect.amazon.model.Region
 import org.http4k.connect.amazon.model.SQSMessage
-import org.http4k.connect.amazon.model.SQSMessageId
 import org.http4k.connect.storage.InMemory
 import org.http4k.connect.storage.Storage
-import org.http4k.core.Body
-import org.http4k.core.ContentType.Companion.APPLICATION_XML
 import org.http4k.core.Method.POST
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.BAD_REQUEST
-import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Uri
-import org.http4k.core.body.form
-import org.http4k.core.extend
-import org.http4k.core.with
-import org.http4k.routing.asRouter
 import org.http4k.routing.bind
-import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.template.HandlebarsTemplates
-import org.http4k.template.viewModel
 import java.time.Clock
-import java.util.UUID
 
 class FakeSQS(
-    private val queues: Storage<List<SQSMessage>> = Storage.InMemory(),
-    private val clock: Clock = Clock.systemDefaultZone(),
-    private val awsAccount: AwsAccount = AwsAccount.of("1234567890")
+    queues: Storage<List<SQSMessage>> = Storage.InMemory(),
+    clock: Clock = Clock.systemDefaultZone(),
+    awsAccount: AwsAccount = AwsAccount.of("1234567890")
 ) : ChaosFake() {
-    private val lens = Body.viewModel(HandlebarsTemplates().CachingClasspath(), APPLICATION_XML).toLens()
 
     override val app = routes(
         "/{account}/{queueName}" bind POST to routes(
-            deleteMessage(),
-            deleteQueue(),
-            receiveMessage(),
-            sendMessage()
+            deleteMessage(queues),
+            deleteQueue(queues),
+            receiveMessage(queues),
+            sendMessage(queues)
         ),
-        "/" bind POST to createQueue()
+        "/" bind POST to createQueue(queues, awsAccount)
     )
 
-    private fun createQueue() = { r: Request -> r.form("Action") == "CreateQueue" }
-        .asRouter() bind { req: Request ->
-        val queueName = req.form("QueueName")!!
-        if (queues.keySet(queueName).isEmpty()) queues[queueName] = listOf()
-
-        Response(OK).with(
-            lens of CreateQueueResponse(
-                req.uri.extend(Uri.of("/$awsAccount/$queueName"))
-            )
-        )
-    }
-
-    private fun deleteQueue() = { r: Request -> r.form("Action") == "DeleteQueue" }
-        .asRouter() bind { req: Request ->
-        val queueName = req.path("queueName")!!
-
-        when {
-            queues.keySet(queueName).isEmpty() -> Response(BAD_REQUEST)
-            else -> Response(OK).with(lens of DeleteQueueResponse)
-        }
-    }
-
-    private fun sendMessage() = { r: Request -> r.form("Action") == "SendMessage" }
-        .asRouter() bind { req: Request ->
-        val queueName = req.path("queueName")!!
-
-        queues[queueName]?.let {
-            val message = req.form("MessageBody")!!
-            val messageId = SQSMessageId.of(queueName + "/" + UUID.randomUUID())
-            val receiptHandle = ReceiptHandle.of(queueName + "/" + UUID.randomUUID())
-            queues[queueName] = it + SQSMessage(messageId, message, message.md5(), receiptHandle, mapOf())
-            Response(OK).with(lens of SendMessageResponse(message, messageId))
-        } ?: Response(BAD_REQUEST)
-    }
-
-    private fun receiveMessage() = { r: Request -> r.form("Action") == "ReceiveMessage" }
-        .asRouter() bind { req: Request ->
-        val queue = queues[req.path("queueName")!!]
-        queue?.let { Response(OK).with(lens of ReceiveMessageResponse(it)) } ?: Response(BAD_REQUEST)
-    }
-
-    private fun deleteMessage() = { r: Request -> r.form("Action") == "DeleteMessage" }
-        .asRouter() bind { req: Request ->
-        val queueName = req.path("queueName")!!
-        val receiptHandle = ReceiptHandle.of(req.form("ReceiptHandle")!!)
-        queues[queueName]?.let {
-            queues[queueName] = it.filterNot { it.receiptHandle == receiptHandle }
-            Response(OK).with(lens of DeleteMessageResponse)
-        } ?: Response(BAD_REQUEST)
-    }
+    private val client = SQS.Http(Region.of("ldn-north-1"), { AwsCredentials("accessKey", "secret") }, this, clock)
 
     /**
      * Convenience function to get a SQS client
      */
-    fun client() = SQS.Http(
-        Region.of("ldn-north-1"),
-        { AwsCredentials("accessKey", "secret") }, this, clock
-    )
+    fun client() = client
 }
 
 fun main() {
---
+++ amazon/sqs/fake/src/main/kotlin/org/http4k/connect/amazon/sqs/endpoints.kt
@@ -0,0 +1,78 @@
+package org.http4k.connect.amazon.sqs
+
+import org.http4k.connect.amazon.model.AwsAccount
+import org.http4k.connect.amazon.model.ReceiptHandle
+import org.http4k.connect.amazon.model.SQSMessage
+import org.http4k.connect.amazon.model.SQSMessageId
+import org.http4k.connect.storage.Storage
+import org.http4k.core.Body
+import org.http4k.core.ContentType
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.Uri
+import org.http4k.core.body.form
+import org.http4k.core.extend
+import org.http4k.core.with
+import org.http4k.routing.asRouter
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.template.HandlebarsTemplates
+import org.http4k.template.viewModel
+import java.util.UUID
+
+fun createQueue(queues: Storage<List<SQSMessage>>, awsAccount: AwsAccount) =
+    { r: Request -> r.form("Action") == "CreateQueue" }
+        .asRouter() bind { req: Request ->
+        val queueName = req.form("QueueName")!!
+        if (queues.keySet(queueName).isEmpty()) queues[queueName] = listOf()
+
+        Response(Status.OK).with(
+            viewModelLens of CreateQueueResponse(
+                req.uri.extend(Uri.of("/$awsAccount/$queueName"))
+            )
+        )
+    }
+
+fun deleteQueue(queues: Storage<List<SQSMessage>>) = { r: Request -> r.form("Action") == "DeleteQueue" }
+    .asRouter() bind { req: Request ->
+    val queueName = req.path("queueName")!!
+
+    when {
+        queues.keySet(queueName).isEmpty() -> Response(Status.BAD_REQUEST)
+        else -> Response(Status.OK).with(viewModelLens of DeleteQueueResponse)
+    }
+}
+
+fun sendMessage(queues: Storage<List<SQSMessage>>) = { r: Request -> r.form("Action") == "SendMessage" }
+    .asRouter() bind { req: Request ->
+    val queueName = req.path("queueName")!!
+
+    queues[queueName]?.let {
+        val message = req.form("MessageBody")!!
+        val messageId = SQSMessageId.of(queueName + "/" + UUID.randomUUID())
+        val receiptHandle = ReceiptHandle.of(queueName + "/" + UUID.randomUUID())
+        queues[queueName] = it + SQSMessage(messageId, message, message.md5(), receiptHandle, mapOf())
+        Response(Status.OK).with(viewModelLens of SendMessageResponse(message, messageId))
+    } ?: Response(Status.BAD_REQUEST)
+}
+
+fun receiveMessage(queues: Storage<List<SQSMessage>>) = { r: Request -> r.form("Action") == "ReceiveMessage" }
+    .asRouter() bind { req: Request ->
+    val queue = queues[req.path("queueName")!!]
+    queue?.let { Response(Status.OK).with(viewModelLens of ReceiveMessageResponse(it)) } ?: Response(Status.BAD_REQUEST)
+}
+
+fun deleteMessage(queues: Storage<List<SQSMessage>>) = { r: Request -> r.form("Action") == "DeleteMessage" }
+    .asRouter() bind { req: Request ->
+    val queueName = req.path("queueName")!!
+    val receiptHandle = ReceiptHandle.of(req.form("ReceiptHandle")!!)
+    queues[queueName]?.let {
+        queues[queueName] = it.filterNot { it.receiptHandle == receiptHandle }
+        Response(Status.OK).with(viewModelLens of DeleteMessageResponse)
+    } ?: Response(Status.BAD_REQUEST)
+}
+
+val viewModelLens by lazy {
+    Body.viewModel(HandlebarsTemplates().CachingClasspath(), ContentType.APPLICATION_XML).toLens()
+}
