82f1dd81b20f006123e4221261dbae455e63c20c
David Denton <denton.david@gmail.com>
2021-01-10 14:51:11 +0000

    convert readme

--- amazon/lambda/fake/src/examples/kotlin/using_connect_client.kt
+++ amazon/lambda/fake/src/examples/kotlin/using_connect_client.kt
@@ -1,11 +1,24 @@
-# Lambda
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.valueOrNull
+import org.http4k.aws.AwsCredentials
+import org.http4k.client.JavaHttpClient
+import org.http4k.connect.RemoteFailure
+import org.http4k.connect.amazon.lambda.FakeLambda
+import org.http4k.connect.amazon.lambda.Http
+import org.http4k.connect.amazon.lambda.Lambda
+import org.http4k.connect.amazon.lambda.action.invokeFunction
+import org.http4k.connect.amazon.model.FunctionName
+import org.http4k.connect.amazon.model.Region
+import org.http4k.core.HttpHandler
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.filter.debug
+import org.http4k.format.Moshi
 
-The Lambda connector provides the following Actions:
+data class Req(val value: String)
+data class Resp(val value: String)
 
-     *  InvokeFunction
-
-### Example usage
-```kotlin
 const val useRealClient = false
 
 fun main() {
@@ -30,14 +43,4 @@ fun main() {
     val resp: Resp = invokeResult.valueOrNull()!!
     println(resp)
 }
-```
-Note that the http4k-connect Fake Lambda implementation is designed to provide a runtime environment for function HttpHandlers that will be invoked directly using the Lambda URL pattern (`https://lambda.${scope.region}.amazonaws.com/2015-03-31/functions/$name/invocations`), rather than being deployed behind APIGateway (where you have total control over the URL pattern where the lambda can be invoked).
-    
-The client APIs utilise the `http4k-aws` module for request signing, which means no dependencies on the incredibly fat Amazon-SDK JARs. This means this integration is perfect for running Serverless Lambdas where binary size is a performance factor.
 
-### Default Fake port: 50322
-
-To start:
-```
-FakeLambda().start()
-```
