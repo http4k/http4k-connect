b14191e460f67968e5404d2e466eff2fd05aeaff
David Denton <denton.david@gmail.com>
2021-01-17 12:03:59 +0000

    revert lost annotation

--- amazon/kms/client/src/main/kotlin/org/http4k/connect/amazon/kms/action/Decrypt.kt
+++ amazon/kms/client/src/main/kotlin/org/http4k/connect/amazon/kms/action/Decrypt.kt
@@ -1,11 +1,12 @@
 package org.http4k.connect.amazon.kms.action
 
+import org.http4k.connect.Http4kConnectAction
 import org.http4k.connect.amazon.model.Base64Blob
 import org.http4k.connect.amazon.model.EncryptionAlgorithm
 import org.http4k.connect.amazon.model.KMSKeyId
 import se.ansman.kotshi.JsonSerializable
 
-
+@Http4kConnectAction
 @JsonSerializable
 data class Decrypt(
     val KeyId: KMSKeyId,
