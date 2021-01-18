d9b853590324e7f1cfd363ab88eba980c6a29e9e
David Denton <denton.david@gmail.com>
2021-01-18 22:49:32 +0000

    fix action name

--- amazon/firehose/client/src/main/kotlin/org/http4k/connect/amazon/firehose/action/FirehoseAction.kt
+++ amazon/firehose/client/src/main/kotlin/org/http4k/connect/amazon/firehose/action/FirehoseAction.kt
@@ -1,10 +1,10 @@
 package org.http4k.connect.amazon.firehose.action
 
 import org.http4k.connect.amazon.AwsJsonAction
-import org.http4k.connect.amazon.firehose.Firehose
 import org.http4k.connect.amazon.firehose.FirehoseMoshi
+import org.http4k.connect.amazon.model.AwsService
 import org.http4k.format.AutoMarshalling
 import kotlin.reflect.KClass
 
 abstract class FirehoseAction<R : Any>(clazz: KClass<R>, autoMarshalling: AutoMarshalling = FirehoseMoshi) :
-    AwsJsonAction<R>(Firehose.awsService, clazz, autoMarshalling)
+    AwsJsonAction<R>(AwsService.of("Firehose_20150804"), clazz, autoMarshalling)
