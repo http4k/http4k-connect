575c66883cbd4f45f2b4f217ff6b5009b4107ad8
David Denton <denton.david@gmail.com>
2021-01-18 18:54:27 +0000

    contract

--- amazon/sns/client/src/test/kotlin/org/http4k/connect/amazon/sns/RealSNSTest.kt
+++ amazon/sns/client/src/test/kotlin/org/http4k/connect/amazon/sns/RealSNSTest.kt
@@ -3,7 +3,8 @@ package org.http4k.connect.amazon.sns
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
+import org.http4k.filter.debug
 
-class RealSNSTest : SNSContract(JavaHttpClient()), RealAwsEnvironment {
+class RealSNSTest : SNSContract(JavaHttpClient().debug()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
 }
--- amazon/sns/client/src/test/kotlin/org/http4k/connect/amazon/sns/SNSContract.kt
+++ amazon/sns/client/src/test/kotlin/org/http4k/connect/amazon/sns/SNSContract.kt
@@ -1,10 +1,54 @@
 package org.http4k.connect.amazon.sns
 
 import org.http4k.connect.amazon.AwsContract
+import org.http4k.connect.amazon.model.Tag
+import org.http4k.connect.amazon.model.TopicName
+import org.http4k.connect.successValue
 import org.http4k.core.HttpHandler
+import org.junit.jupiter.api.Test
+import java.util.UUID
 
 abstract class SNSContract(http: HttpHandler) : AwsContract(http) {
     private val sns by lazy {
         SNS.Http(aws.region, { aws.credentials }, http)
+    }
+    private val topicName = TopicName.of(UUID.randomUUID().toString())
+
+    @Test
+    fun `topic lifecycle`() {
+        with(sns) {
+            val topicArn = createTopic(
+                topicName,
+                listOf(Tag("key", "value")),
+                mapOf("MaximumMessageSize" to "10000")
+            ).successValue().topicArn
+            try {
+//                val id = sendMessage(
+//                    accountId, queueName, "hello world", expires = expires,
+//                    attributes = listOf(
+//                        MessageAttribute("foo", "123", org.http4k.connect.amazon.model.DataType.Number),
+//                        MessageAttribute("bar", "123", org.http4k.connect.amazon.model.DataType.Number),
+//                        MessageAttribute("binaryfoo", org.http4k.connect.amazon.model.Base64Blob.encoded("foobar"))
+//                    ),
+//                    systemAttributes = listOf(
+//                        MessageSystemAttribute(
+//                            "AWSTraceHeader",
+//                            "Root=1-5f4a4a2d-b94f96db34d41be1349080d2",
+//                            org.http4k.connect.amazon.model.DataType.String
+//                        )
+//                    )
+//                ).successValue().MessageId
+//
+//                val received = receiveMessage(accountId, queueName).successValue().first()
+//                assertThat(received.messageId, equalTo(id))
+//                assertThat(received.body, equalTo("hello world"))
+//
+//                deleteMessage(accountId, queueName, received.receiptHandle).successValue()
+//
+//                assertThat(receiveMessage(accountId, queueName).successValue().size, equalTo(0))
+            } finally {
+                deleteTopic(topicArn).successValue()
+            }
+        }
     }
 }
