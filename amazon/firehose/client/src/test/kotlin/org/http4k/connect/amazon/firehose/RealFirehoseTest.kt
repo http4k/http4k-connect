704fcb4f3fc14b700977bbae9ad0d804f334708f
David Denton <denton.david@gmail.com>
2021-01-18 22:59:17 +0000

    add setup for firehose

--- amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/RealFirehoseTest.kt
+++ amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/RealFirehoseTest.kt
@@ -3,7 +3,8 @@ package org.http4k.connect.amazon.firehose
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
+import org.http4k.filter.debug
 
-class RealFirehoseTest : FirehoseContract(JavaHttpClient()), RealAwsEnvironment {
+class RealFirehoseTest : FirehoseContract(JavaHttpClient().debug()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
 }
