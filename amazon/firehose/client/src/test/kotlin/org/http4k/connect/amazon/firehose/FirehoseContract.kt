78d4b1b8338339c6419888a53e3086dd9b896007
David Denton <denton.david@gmail.com>
2021-01-19 09:32:18 +0000

    add listing of delivery streams

--- amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/FirehoseContract.kt
+++ amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/FirehoseContract.kt
@@ -1,5 +1,7 @@
 package org.http4k.connect.amazon.firehose
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.http4k.connect.amazon.AwsContract
 import org.http4k.connect.amazon.firehose.action.Record
 import org.http4k.connect.amazon.model.Base64Blob
@@ -8,18 +10,18 @@ import org.http4k.connect.randomString
 import org.http4k.connect.successValue
 import org.http4k.core.HttpHandler
 import org.junit.jupiter.api.Test
-import java.util.UUID
 
 abstract class FirehoseContract(http: HttpHandler) : AwsContract(http) {
     private val firehose by lazy {
         Firehose.Http(aws.region, { aws.credentials }, http)
     }
 
-    private val deliveryStreamName = DeliveryStreamName.of(UUID.randomUUID().toString())
+    private val deliveryStreamName = DeliveryStreamName.of("connect")
 
     @Test
     fun `send records`() {
         with(firehose) {
+            assertThat(listDeliveryStreams().successValue().DeliveryStreamNames.contains(deliveryStreamName), equalTo(true))
             putRecord(
                 deliveryStreamName,
                 Record(Base64Blob.encoded(randomString))
