e9741b53ca9f655923359bb953b2fdc3e4635163
David Denton <denton.david@gmail.com>
2021-01-30 17:22:18 +0000

    add action adapters to turn non-result monads into result monads

--- core/client/build.gradle.kts
+++ core/client/build.gradle.kts
@@ -3,3 +3,9 @@ dependencies {
     compileOnly("org.http4k:http4k-format-moshi")
     testCompileOnly("org.http4k:http4k-format-moshi")
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + "-Xallow-result-return-type"
+    }
+}
