7f6931c3f478d74394568572c0b437cb1c5bf610
David Denton <denton.david@gmail.com>
2021-02-03 19:15:12 +0000

    upgrade http4k, fix generator to not hardcode result type

--- http4k-connect-kapt-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterProcessor.kt
+++ http4k-connect-kapt-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterProcessor.kt
@@ -4,16 +4,12 @@ import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterSpec
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.asTypeName
 import com.squareup.kotlinpoet.metadata.ImmutableKmClass
 import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
 import com.squareup.kotlinpoet.metadata.isNullable
 import com.squareup.kotlinpoet.metadata.isPrivate
-import dev.forkhandles.result4k.Result
 import org.http4k.connect.Http4kConnectAction
 import org.http4k.connect.Http4kConnectAdapter
-import org.http4k.connect.RemoteFailure
 import org.http4k.connect.plugin.Http4kConnectProcessor.Companion.KAPT_KOTLIN_GENERATED_OPTION_NAME
 import java.io.File
 import javax.annotation.processing.RoundEnvironment
@@ -52,33 +48,22 @@ class Http4kConnectAdapterProcessor : Http4kConnectProcessor() {
 }
 
 @KotlinPoetMetadataPreview
-private fun ImmutableKmClass.generateActionFunctions(it: ImmutableKmClass): List<FunSpec> {
-    val message = it.supertypes.first().arguments.first().type!!.generifiedType()
-    val actionClazz = it.name.name()
-
-    val actionClassName = it.name.asClassName()
-
-    return it.constructors
-        .filterNot { it.isPrivate }
-        .map {
-            val baseFunction = FunSpec.builder(actionClazz.decapitalize())
-                .receiver(name.asClassName())
-                .addCode(
-                    CodeBlock.of(
-                        "return this(%T(${it.valueParameters.joinToString(", ") { it.name.decapitalize() }}))",
-                        actionClassName
-                    )
+private fun ImmutableKmClass.generateActionFunctions(clazz: ImmutableKmClass) = clazz.constructors
+    .filterNot { it.isPrivate }
+    .map {
+        val baseFunction = FunSpec.builder(clazz.name.name().decapitalize())
+            .receiver(name.asClassName())
+            .addCode(
+                CodeBlock.of(
+                    "return this(%T(${it.valueParameters.joinToString(", ") { it.name.decapitalize() }}))",
+                    clazz.name.asClassName()
                 )
-                .returns(
-                    Result::class.asTypeName()
-                        .parameterizedBy(listOf(message, RemoteFailure::class.asTypeName()))
-                )
+            )
 
-            it.valueParameters.forEach {
-                val base = ParameterSpec.builder(it.name.decapitalize(), it.type!!.generifiedType())
-                if (it.type!!.isNullable) base.defaultValue(CodeBlock.of("null"))
-                baseFunction.addParameter(base.build())
-            }
-            baseFunction.build()
+        it.valueParameters.forEach {
+            val base = ParameterSpec.builder(it.name.decapitalize(), it.type!!.generifiedType())
+            if (it.type!!.isNullable) base.defaultValue(CodeBlock.of("null"))
+            baseFunction.addParameter(base.build())
         }
-}
+        baseFunction.build()
+    }
