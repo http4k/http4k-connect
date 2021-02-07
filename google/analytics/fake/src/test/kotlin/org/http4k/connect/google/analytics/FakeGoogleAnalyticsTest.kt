3fb38fc335951affda8b621a2bd744c9c2a8d531
David Denton <denton.david@gmail.com>
2021-02-07 18:01:10 +0000

    use convenient pageview method

--- google/analytics/fake/src/test/kotlin/org/http4k/connect/google/analytics/FakeGoogleAnalyticsTest.kt
+++ google/analytics/fake/src/test/kotlin/org/http4k/connect/google/analytics/FakeGoogleAnalyticsTest.kt
@@ -4,7 +4,6 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import dev.forkhandles.result4k.Success
 import org.http4k.connect.google.FakeGoogleAnalytics
-import org.http4k.connect.google.analytics.action.PageView
 import org.http4k.connect.google.model.ClientId
 import org.http4k.connect.google.model.TrackingId
 import org.junit.jupiter.api.Test
@@ -15,15 +14,13 @@ class FakeGoogleAnalyticsTest {
     @Test
     fun `can log page view`() {
         assertThat(
-            analytics(
-                PageView(
-                    "some-user-agent",
-                    ClientId.of("SOME_CLIENT_ID"),
-                    "title",
-                    "/doc/path",
-                    "www.http4k.org",
-                    TrackingId.of("SOME_TRACKING_ID")
-                )
+            analytics.pageView(
+                "some-user-agent",
+                ClientId.of("SOME_CLIENT_ID"),
+                "title",
+                "/doc/path",
+                "www.http4k.org",
+                TrackingId.of("SOME_TRACKING_ID")
             ),
             equalTo(Success(Unit))
         )
