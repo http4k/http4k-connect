4f44d72b66732bd7bcd5996b9e46f57faca12cb4
David Denton <denton.david@gmail.com>
2021-02-14 19:26:43 +0000

    tweak debug filter

--- storage/core/src/main/kotlin/org/http4k/connect/storage/debug.kt
+++ storage/core/src/main/kotlin/org/http4k/connect/storage/debug.kt
@@ -4,7 +4,7 @@ package org.http4k.connect.storage
  * Debug all inputs and outputs of storage calls.
  */
 fun <T : Any> Storage<T>.debug(printFn: (String) -> Unit = ::println) = object : Storage<T> by this {
-    private fun println(s: String) = printFn(this@debug::class.simpleName + " > " + s)
+    private fun println(s: String) = printFn(this@debug.javaClass.name + " > " + s)
 
     override fun get(key: String) = this@debug[key].also { println("get: $key > $it") }
 
