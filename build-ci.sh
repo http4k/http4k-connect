788e2713a8ada29813d80354c03e400fc7ec91ea
Ivan Sanchez <ivan@gourame.com>
2021-03-20 13:50:00 +0000

    Build in parallel

--- build-ci.sh
+++ build-ci.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew check jacocoRootReport --build-cache
+./gradlew check jacocoRootReport --build-cache --parallel
 bash <(curl -s https://codecov.io/bash)
