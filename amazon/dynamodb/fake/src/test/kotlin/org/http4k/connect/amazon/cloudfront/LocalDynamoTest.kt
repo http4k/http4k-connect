20076ad6d6ed9f08b419669226f9ff6e797a39b5
David Denton <denton.david@gmail.com>
2021-03-28 20:36:49 +0100

    add dynamo local test

--- amazon/dynamodb/fake/src/test/kotlin/org/http4k/connect/amazon/cloudfront/LocalDynamoTest.kt
+++ amazon/dynamodb/fake/src/test/kotlin/org/http4k/connect/amazon/cloudfront/LocalDynamoTest.kt
@@ -1,15 +1,14 @@
 package org.http4k.connect.amazon.cloudfront
 
-import org.http4k.aws.AwsCredentials
 import org.http4k.client.JavaHttpClient
-import org.http4k.connect.amazon.AwsEnvironment
 import org.http4k.connect.amazon.dynamodb.DynamoDbContract
-import org.http4k.connect.amazon.model.Region
+import org.http4k.connect.amazon.fakeAwsEnvironment
 import org.http4k.connect.assumeDockerDaemonRunning
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.filter.ClientFilters.SetBaseUriFrom
-import org.testcontainers.dynamodb.DynaliteContainer
+import org.http4k.filter.debug
+import org.testcontainers.containers.GenericContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName.parse
@@ -17,36 +16,19 @@ import java.time.Duration
 
 @Testcontainers
 class LocalDynamoTest : DynamoDbContract(Duration.ofSeconds(1)) {
-
     init {
         assumeDockerDaemonRunning()
     }
 
     override val http by lazy {
-        SetBaseUriFrom(Uri.of(dynamo.endpointConfiguration.serviceEndpoint))
-            .then(JavaHttpClient())
+        SetBaseUriFrom(Uri.of("http://localhost:${dynamo.getMappedPort(8000)}"))
+            .then(JavaHttpClient().debug())
     }
 
     @Container
-    val dynamo =
-        DynaliteContainer(parse("quay.io/testcontainers/dynalite:v1.2.1-1")).apply {
-            start()
-        }
+    val dynamo = GenericContainer<GenericContainer<*>>(
+        parse("amazon/dynamodb-local:1.15.0")
+    ).withExposedPorts(8000)
 
-    override val aws: AwsEnvironment by lazy {
-        val credentials = dynamo.credentials.credentials
-        AwsEnvironment(
-            AwsCredentials(credentials.awsAccessKeyId, credentials.awsSecretKey),
-            Region.of("us-east-1")
-        )
-    }
-
-
-    override fun `transactional items`() {
-        // not supported by dynalite
-    }
-
-    override fun `partiSQL operations`() {
-        // not supported by dynalite
-    }
+    override val aws = fakeAwsEnvironment
 }
