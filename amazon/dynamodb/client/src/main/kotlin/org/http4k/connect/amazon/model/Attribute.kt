67e66a96b14e3b2bfbfd0422bba8cbbfed16573d
David Denton <denton.david@gmail.com>
2021-03-29 13:59:55 +0100

    tweak readme

--- amazon/dynamodb/client/src/main/kotlin/org/http4k/connect/amazon/model/Attribute.kt
+++ amazon/dynamodb/client/src/main/kotlin/org/http4k/connect/amazon/model/Attribute.kt
@@ -1,12 +1,27 @@
 package org.http4k.connect.amazon.model
 
 import org.http4k.connect.amazon.dynamodb.action.AttributeValue
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.B
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.BOOL
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.BS
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.L
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.M
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.N
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.NS
+import org.http4k.connect.amazon.dynamodb.action.DynamoDataType.S
 import org.http4k.connect.amazon.dynamodb.action.Item
 import org.http4k.lens.BiDiLens
 import org.http4k.lens.BiDiLensSpec
 import org.http4k.lens.BiDiMapping
+import org.http4k.lens.Lens
+import org.http4k.lens.LensFailure
 import org.http4k.lens.LensGet
+import org.http4k.lens.LensInjector
 import org.http4k.lens.LensSet
+import org.http4k.lens.LensSpec
+import org.http4k.lens.Meta
+import org.http4k.lens.Missing
 import org.http4k.lens.ParamMeta.ObjectParam
 import org.http4k.lens.StringBiDiMappings
 import java.time.format.DateTimeFormatter
@@ -22,7 +37,7 @@ import java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME
  * can be used to inject values with Item(attr of "123") or extract with attr(item).
  */
 object Attribute {
-    private val base = AttrLensSpec(
+    private fun base(dataType: DynamoDataType) = AttrLensSpec(dataType,
         LensGet { name, target ->
             target[AttributeName.of(name)]
                 ?.takeIf { it.NULL != true }
@@ -36,29 +51,30 @@ object Attribute {
         }
     )
 
-    fun list() = base.map({ it.L!! }, { AttributeValue.List(it) })
-    fun map() = base.map({ it.M!! }, { AttributeValue.Map(it) })
-    fun string() = base.map({ it.S!! }, AttributeValue.Companion::Str)
-    fun strings() = base.map({ it.SS!! }, { AttributeValue.StrSet(it) })
-    fun nonEmptyString() =
-        base.map({ it.S!!.takeIf(String::isNotBlank) ?: error("blank string") }, AttributeValue.Companion::Str)
+    fun list() = base(L).map({ it.L!! }, { AttributeValue.List(it) })
+    fun map() = base(M).map({ it.M!! }, { AttributeValue.Map(it) })
+    fun string() = base(S).map({ it.S!! }, AttributeValue::Str)
+    fun strings() = base(L).map({ it.SS!! }, { AttributeValue.StrSet(it) })
 
-    fun int() = base.map({ it.N!!.toString().toInt() }, AttributeValue.Companion::Num)
-    fun numbers() = base.map({ it.NS!!.map(String::toBigDecimal).toSet() }, AttributeValue.Companion::NumSet)
-    fun ints() = base.map({ it.NS!!.map(String::toInt).toSet() }, AttributeValue.Companion::NumSet)
-    fun long() = base.map({ it.N!!.toString().toLong() }, AttributeValue.Companion::Num)
-    fun longs() = base.map({ it.NS!!.map(String::toLong).toSet() }, AttributeValue.Companion::NumSet)
-    fun double() = base.map({ it.N!!.toString().toDouble() }, AttributeValue.Companion::Num)
-    fun doubles() = base.map({ it.NS!!.map(String::toDouble).toSet() }, AttributeValue.Companion::NumSet)
-    fun float() = base.map({ it.N!!.toString().toFloat() }, AttributeValue.Companion::Num)
-    fun floats() = base.map({ it.NS!!.map(String::toFloat).toSet() }, AttributeValue.Companion::NumSet)
-    fun boolean() = base.map({ it.BOOL!! }, AttributeValue.Companion::Bool)
-    fun base64Blob() = base.map({ it.B!! }, { AttributeValue.Base64(it) })
-    fun base64Blobs() = base.map({ it.BS!! }, { AttributeValue.Base64Set(it) })
-    fun bigDecimal() = base.map({ it.N!!.toString().toBigDecimal() }, AttributeValue.Companion::Num)
-    fun bigDecimals() = base.map({ it.NS!!.map(String::toBigDecimal).toSet() }, AttributeValue.Companion::NumSet)
-    fun bigInteger() = base.map({ it.N!!.toString().toBigInteger() }, AttributeValue.Companion::Num)
-    fun bigIntegers() = base.map({ it.NS!!.map(String::toBigInteger).toSet() }, AttributeValue.Companion::NumSet)
+    fun nonEmptyString() =
+        base(S).map({ it.S!!.takeIf(String::isNotBlank) ?: error("blank string") }, AttributeValue::Str)
+
+    fun int() = base(N).map({ it.N!!.toString().toInt() }, AttributeValue::Num)
+    fun numbers() = base(N).map({ it.NS!!.map(String::toBigDecimal).toSet() }, AttributeValue::NumSet)
+    fun ints() = base(NS).map({ it.NS!!.map(String::toInt).toSet() }, AttributeValue::NumSet)
+    fun long() = base(L).map({ it.N!!.toString().toLong() }, AttributeValue::Num)
+    fun longs() = base(NS).map({ it.NS!!.map(String::toLong).toSet() }, AttributeValue::NumSet)
+    fun double() = base(N).map({ it.N!!.toString().toDouble() }, AttributeValue::Num)
+    fun doubles() = base(NS).map({ it.NS!!.map(String::toDouble).toSet() }, AttributeValue::NumSet)
+    fun float() = base(N).map({ it.N!!.toString().toFloat() }, AttributeValue::Num)
+    fun floats() = base(NS).map({ it.NS!!.map(String::toFloat).toSet() }, AttributeValue::NumSet)
+    fun boolean() = base(BOOL).map({ it.BOOL!! }, AttributeValue::Bool)
+    fun base64Blob() = base(B).map({ it.B!! }, { AttributeValue.Base64(it) })
+    fun base64Blobs() = base(BS).map({ it.BS!! }, { AttributeValue.Base64Set(it) })
+    fun bigDecimal() = base(N).map({ it.N!!.toString().toBigDecimal() }, AttributeValue::Num)
+    fun bigDecimals() = base(NS).map({ it.NS!!.map(String::toBigDecimal).toSet() }, AttributeValue::NumSet)
+    fun bigInteger() = base(N).map({ it.N!!.toString().toBigInteger() }, AttributeValue::Num)
+    fun bigIntegers() = base(NS).map({ it.NS!!.map(String::toBigInteger).toSet() }, AttributeValue::NumSet)
     fun uuid() = string().map(StringBiDiMappings.uuid())
     fun uri() = string().map(StringBiDiMappings.uri())
     fun duration() = string().map(StringBiDiMappings.duration())
@@ -82,7 +98,7 @@ object Attribute {
     fun offsetDateTime(formatter: DateTimeFormatter = ISO_OFFSET_DATE_TIME) =
         string().map(StringBiDiMappings.zonedDateTime(formatter))
 
-    fun timestamp() = long().map(Timestamp.Companion::of, Timestamp::value)
+    fun timestamp() = long().map(Timestamp::of, Timestamp::value)
 
     inline fun <reified T : Enum<T>> enum() = string().map(StringBiDiMappings.enum<T>())
 }
@@ -91,10 +107,51 @@ fun <NEXT, IN : Any, OUT> BiDiLensSpec<IN, OUT>.map(mapping: BiDiMapping<OUT, NE
     map(mapping::invoke, mapping::invoke)
 
 open class AttrLensSpec<OUT>(
-    get: LensGet<Item, OUT>,
-    setter: LensSet<Item, OUT>
-) : BiDiLensSpec<Item, OUT>("item", ObjectParam, get, setter) {
-    override val multi get() = throw UnsupportedOperationException("use other methods")
+    private val dataType: DynamoDataType,
+    private val get: LensGet<Item, OUT>,
+    private val set: LensSet<Item, OUT>
+) : LensSpec<Item, OUT>("item", ObjectParam, get) {
+    fun <NEXT> map(nextIn: (OUT) -> NEXT, nextOut: (NEXT) -> OUT) =
+        AttrLensSpec(dataType, get.map(nextIn), set.map(nextOut))
+
+    fun <NEXT> map(mapping: BiDiMapping<OUT, NEXT>) = map(mapping::invoke, mapping::invoke)
+
+    override fun optional(name: String, description: String?): AttrLens<OUT?> {
+        val getLens = get(name)
+        val setLens = set(name)
+        return AttrLens(
+            dataType,
+            Meta(false, location, paramMeta, name, description),
+            { getLens(it).run { if (isEmpty()) null else first() } },
+            { out: OUT?, target -> setLens(out?.let { listOf(it) } ?: emptyList(), target) }
+        )
+    }
+
+    override fun required(name: String, description: String?): AttrLens<OUT> {
+        val getLens = get(name)
+        val setLens = set(name)
+        return AttrLens(
+            dataType,
+            Meta(true, location, paramMeta, name, description),
+            {
+                getLens(it).firstOrNull()
+                    ?: throw LensFailure(Missing(Meta(true, location, paramMeta, name, description)), target = it)
+            },
+            { out: OUT, target -> setLens(listOf(out), target) })
+    }
+}
+
+class AttrLens<FINAL>(
+    val dataType: DynamoDataType,
+    meta: Meta,
+    get: (Item) -> FINAL,
+    private val lensSet: (FINAL, Item) -> Item
+) : LensInjector<FINAL, Item>, Lens<Item, FINAL>(meta, get) {
+
+    fun value(value: FINAL): AttributeValue = TODO()
+
+    @Suppress("UNCHECKED_CAST")
+    override operator fun <R : Item> invoke(value: FINAL, target: R): R = lensSet(value, target) as R
 }
 
 val <FINAL> BiDiLens<Item, FINAL>.name get() = AttributeName.of(meta.name)
