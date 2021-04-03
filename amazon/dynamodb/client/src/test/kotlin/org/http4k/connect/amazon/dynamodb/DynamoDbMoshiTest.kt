803376213a64bedb6bbae2b997c9e4fd622a7f10
David Denton <denton.david@gmail.com>
2021-04-03 14:22:18 +0100

    use generated adapter factories

--- amazon/dynamodb/client/src/test/kotlin/org/http4k/connect/amazon/dynamodb/DynamoDbMoshiTest.kt
+++
@@ -1,5 +0,0 @@
-package org.http4k.connect.amazon.dynamodb
-
-import org.http4k.connect.SystemMoshiContract
-
-class DynamoDbMoshiTest : SystemMoshiContract(DynamoDbMoshi)
--- amazon/secretsmanager/client/src/main/kotlin/org/http4k/connect/amazon/secretsmanager/SecretsManagerMoshi.kt
+++ amazon/secretsmanager/client/src/main/kotlin/org/http4k/connect/amazon/secretsmanager/SecretsManagerMoshi.kt
@@ -1,36 +1,23 @@
 package org.http4k.connect.amazon.secretsmanager
 
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import org.http4k.connect.amazon.model.SecretId
 import org.http4k.connect.amazon.model.VersionId
 import org.http4k.connect.amazon.model.VersionStage
-import org.http4k.connect.amazon.secretsmanager.action.KotshiCreateSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiCreatedSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiDeleteSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiDeletedSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiFilterJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiGetSecretValueJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiListSecretsJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiPutSecretValueJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiRotationRulesJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiSecretValueJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiSecretsJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiUpdateSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiUpdatedSecretJsonAdapter
-import org.http4k.connect.amazon.secretsmanager.action.KotshiUpdatedSecretValueJsonAdapter
 import org.http4k.format.AutoMappingConfiguration
 import org.http4k.format.AwsJsonAdapterFactory
 import org.http4k.format.ConfigurableMoshi
-import org.http4k.format.adapter
 import org.http4k.format.asConfigurable
 import org.http4k.format.value
 import org.http4k.format.withAwsCoreMappings
 import org.http4k.format.withStandardMappings
+import se.ansman.kotshi.KotshiJsonAdapterFactory
 
 object SecretsManagerMoshi : ConfigurableMoshi(
     Moshi.Builder()
-        .add(SecretsManagerJsonAdapterFactory)
+        .add(KotshiSecretsManagerJsonAdapterFactory)
+        .add(AwsJsonAdapterFactory())
         .asConfigurable()
         .withStandardMappings()
         .withAwsCoreMappings()
@@ -44,20 +31,6 @@ fun <T> AutoMappingConfiguration<T>.withSecretsManagerMappings() = apply {
     value(VersionStage)
 }
 
-object SecretsManagerJsonAdapterFactory : AwsJsonAdapterFactory(
-    adapter(::KotshiCreatedSecretJsonAdapter),
-    adapter(::KotshiCreateSecretJsonAdapter),
-    adapter(::KotshiDeletedSecretJsonAdapter),
-    adapter(::KotshiDeleteSecretJsonAdapter),
-    adapter(::KotshiFilterJsonAdapter),
-    adapter(::KotshiGetSecretValueJsonAdapter),
-    adapter(::KotshiListSecretsJsonAdapter),
-    adapter(::KotshiPutSecretValueJsonAdapter),
-    adapter { KotshiRotationRulesJsonAdapter() },
-    adapter(::KotshiSecretJsonAdapter),
-    adapter(::KotshiSecretsJsonAdapter),
-    adapter(::KotshiSecretValueJsonAdapter),
-    adapter(::KotshiUpdatedSecretJsonAdapter),
-    adapter(::KotshiUpdatedSecretValueJsonAdapter),
-    adapter(::KotshiUpdateSecretJsonAdapter),
-)
+@KotshiJsonAdapterFactory
+abstract class SecretsManagerJsonAdapterFactory : JsonAdapter.Factory
+
