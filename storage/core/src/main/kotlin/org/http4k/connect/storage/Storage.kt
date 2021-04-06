3d16d44d57252873e6915e21e5d92c80740cc49e
David Denton <denton.david@gmail.com>
2021-04-06 16:19:58 +0100

    add default to storage

--- storage/core/src/main/kotlin/org/http4k/connect/storage/Storage.kt
+++ storage/core/src/main/kotlin/org/http4k/connect/storage/Storage.kt
@@ -8,7 +8,7 @@ interface Storage<T : Any> {
     operator fun get(key: String): T?
     operator fun set(key: String, data: T)
     fun remove(key: String): Boolean
-    fun keySet(keyPrefix: String): Set<String>
+    fun keySet(keyPrefix: String = ""): Set<String>
     fun removeAll(keyPrefix: String = ""): Boolean
 
     companion object
