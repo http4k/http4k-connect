ca6cd2a7ebefe40292182df1d15e833ad9d51f7a
David Denton <denton.david@gmail.com>
2021-04-07 19:05:14 +0100

    repackage all the things

--- amazon/secretsmanager/fake/src/examples/kotlin/using_connect_client.kt
+++ amazon/secretsmanager/fake/src/examples/kotlin/using_connect_client.kt
@@ -8,6 +8,7 @@ import org.http4k.connect.amazon.secretsmanager.FakeSecretsManager
 import org.http4k.connect.amazon.secretsmanager.Http
 import org.http4k.connect.amazon.secretsmanager.SecretsManager
 import org.http4k.connect.amazon.secretsmanager.action.CreatedSecret
+import org.http4k.connect.amazon.secretsmanager.createSecret
 import org.http4k.connect.amazon.secretsmanager.getSecretValue
 import org.http4k.connect.amazon.secretsmanager.model.SecretId
 import org.http4k.core.HttpHandler
