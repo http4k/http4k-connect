94512214fb29cee72f5bf68950921fe9883287b6
David Denton <denton.david@gmail.com>
2021-04-20 14:45:42 +0100

    add headers to the PutObject action

--- amazon/s3/client/src/main/kotlin/org/http4k/connect/amazon/s3/action/PutObject.kt
+++ amazon/s3/client/src/main/kotlin/org/http4k/connect/amazon/s3/action/PutObject.kt
@@ -5,6 +5,7 @@ import dev.forkhandles.result4k.Success
 import org.http4k.connect.Http4kConnectAction
 import org.http4k.connect.RemoteFailure
 import org.http4k.connect.amazon.s3.model.BucketKey
+import org.http4k.core.Headers
 import org.http4k.core.Method.PUT
 import org.http4k.core.Request
 import org.http4k.core.Response
@@ -12,8 +13,9 @@ import org.http4k.core.Uri
 import java.io.InputStream
 
 @Http4kConnectAction
-data class PutObject(val key: BucketKey, val content: InputStream) : S3BucketAction<Unit> {
-    override fun toRequest() = Request(PUT, uri()).body(content)
+data class PutObject(val key: BucketKey, val content: InputStream, val headers: Headers = emptyList()) :
+    S3BucketAction<Unit> {
+    override fun toRequest() = Request(PUT, uri()).headers(headers).body(content)
 
     override fun toResult(response: Response) = with(response) {
         when {
