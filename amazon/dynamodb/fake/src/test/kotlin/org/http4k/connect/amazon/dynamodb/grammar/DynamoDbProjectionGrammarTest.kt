64560bf1d3dbf82c812771b4b9a339241effdaa6
David Denton <denton.david@gmail.com>
2021-05-07 17:59:36 +0100

    projection expression names

--- amazon/dynamodb/fake/src/test/kotlin/org/http4k/connect/amazon/dynamodb/grammar/DynamoDbProjectionGrammarTest.kt
+++ amazon/dynamodb/fake/src/test/kotlin/org/http4k/connect/amazon/dynamodb/grammar/DynamoDbProjectionGrammarTest.kt
@@ -3,6 +3,7 @@ package org.http4k.connect.amazon.dynamodb.grammar
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.connect.amazon.dynamodb.model.Attribute
+import org.http4k.connect.amazon.dynamodb.model.AttributeName
 import org.http4k.connect.amazon.dynamodb.model.Item
 import org.junit.jupiter.api.Test
 
@@ -22,6 +23,14 @@ class DynamoDbProjectionGrammarTest {
     }
 
     @Test
+    fun `attribute value with expression name`() {
+        assertThat(
+            DynamoDbProjectionGrammar.parse("#att").eval(ItemWithSubstitutions(Item(attr1 of "123"), mapOf("#att" to AttributeName.of("attr1")))),
+            equalTo(listOf(attr1.name to attr1.asValue("123")))
+        )
+    }
+
+    @Test
     fun `indexed attribute value`() {
         assertThat(
             DynamoDbProjectionGrammar.parse("attrList[1]").eval(
@@ -35,6 +44,27 @@ class DynamoDbProjectionGrammarTest {
                             )
                         )
                     )
+                )
+            ),
+            equalTo(listOf(attrList.name to attrList.asValue(listOf(attrList.asValue(listOf(attrNum.asValue(456)))))))
+        )
+    }
+
+    @Test
+    fun `indexed attribute value with expression name`() {
+        assertThat(
+            DynamoDbProjectionGrammar.parse("#theList[1]").eval(
+                ItemWithSubstitutions(
+                    Item(
+                        attr1 of "DON'T GET ME",
+                        attrList of listOf(
+                            attr1.asValue("123"),
+                            attrList.asValue(
+                                listOf(attrNum.asValue(456))
+                            )
+                        )
+                    ),
+                    mapOf("#theList" to AttributeName.of("attrList"))
                 )
             ),
             equalTo(listOf(attrList.name to attrList.asValue(listOf(attrList.asValue(listOf(attrNum.asValue(456)))))))
@@ -92,6 +122,16 @@ class DynamoDbProjectionGrammarTest {
         assertThat(
             DynamoDbProjectionGrammar.parse("attrMap.attr1").eval(
                 ItemWithSubstitutions(Item(attrMap of Item(attr1 of "456", attrNum of 456)))
+            ),
+            equalTo(listOf(attrMap.name to attrMap.asValue(Item(attr1 of "456"))))
+        )
+    }
+
+    @Test
+    fun `map attribute value with expression name`() {
+        assertThat(
+            DynamoDbProjectionGrammar.parse("#map.#att").eval(
+                ItemWithSubstitutions(Item(attrMap of Item(attr1 of "456", attrNum of 456)), mapOf("#map" to AttributeName.of("attrMap"), "#att" to AttributeName.of("attr1")))
             ),
             equalTo(listOf(attrMap.name to attrMap.asValue(Item(attr1 of "456"))))
         )
