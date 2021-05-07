653df336830737605943e15027578dc31485844a
David Denton <denton.david@gmail.com>
2021-05-07 13:01:01 +0100

    add precedence for index and mapped

--- amazon/dynamodb/fake/src/main/kotlin/org/http4k/connect/amazon/dynamodb/grammar/DynamoDbProjectionGrammar.kt
+++ amazon/dynamodb/fake/src/main/kotlin/org/http4k/connect/amazon/dynamodb/grammar/DynamoDbProjectionGrammar.kt
@@ -3,6 +3,7 @@ package org.http4k.connect.amazon.dynamodb.grammar
 import parser4k.OutputCache
 import parser4k.Parser
 import parser4k.oneOf
+import parser4k.oneOfWithPrecedence
 import parser4k.parseWith
 import parser4k.reset
 import parser4k.with
@@ -15,10 +16,12 @@ object DynamoDbProjectionGrammar {
     private val expr: Parser<Expr> =
         oneOf(
             ProjectionList(::expr).with(cache),
-            oneOf(
-                ExpressionAttributeName(::expr).with(cache),
+            oneOfWithPrecedence(
                 ProjectionMapAttributeValue(::expr).with(cache),
                 ProjectionIndexedAttributeValue(::expr).with(cache),
+            ),
+            oneOf(
+                ExpressionAttributeName(::expr).with(cache),
                 ExpressionAttributeValue(::expr).with(cache),
                 ProjectionAttributeValue(::expr).with(cache)
             )
