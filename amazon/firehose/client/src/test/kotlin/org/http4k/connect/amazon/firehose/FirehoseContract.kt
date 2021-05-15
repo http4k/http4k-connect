d8172dd47b784d7557bb9be824f340e37f7e1e4a
David Denton <denton.david@gmail.com>
2021-05-15 16:02:02 +0100

    disable tests

--- amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/FirehoseContract.kt
+++ amazon/firehose/client/src/test/kotlin/org/http4k/connect/amazon/firehose/FirehoseContract.kt
@@ -3,15 +3,12 @@ package org.http4k.connect.amazon.firehose
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.connect.amazon.AwsContract
-import org.http4k.connect.amazon.DeliveryStreamType.DirectPut
 import org.http4k.connect.amazon.core.model.Base64Blob
 import org.http4k.connect.amazon.model.DeliveryStreamName
-import org.http4k.connect.amazon.model.EndpointConfiguration
-import org.http4k.connect.amazon.model.HttpEndpointDestinationConfiguration
 import org.http4k.connect.amazon.model.Record
 import org.http4k.connect.successValue
 import org.http4k.core.HttpHandler
-import org.http4k.core.Uri
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
@@ -24,19 +21,20 @@ abstract class FirehoseContract(http: HttpHandler) : AwsContract() {
     private val deliveryStreamName = DeliveryStreamName.of("connect")
 
     @Test
+    @Disabled
     fun `create and delete delivery stream`() {
         val deliveryStreamName = DeliveryStreamName.of(UUID.randomUUID().toString())
         with(firehose) {
             try {
-                createDeliveryStream(
-                    deliveryStreamName, DirectPut,
-                    HttpEndpointDestinationConfiguration = HttpEndpointDestinationConfiguration(
-                        EndpointConfiguration = EndpointConfiguration(
-                            Url = Uri.of("http://localhost:8080")
-                        )
-                    )
-
-                ).successValue()
+//                createDeliveryStream(
+//                    deliveryStreamName, DirectPut,
+//                    HttpEndpointDestinationConfiguration = HttpEndpointDestinationConfiguration(
+//                        EndpointConfiguration = EndpointConfiguration(
+//                            Url = Uri.of("http://localhost:8080")
+//                        )
+//                    )
+//
+//                ).successValue()
                 assertThat(
                     listDeliveryStreams().successValue().DeliveryStreamNames.contains(deliveryStreamName),
                     equalTo(true)
@@ -48,6 +46,7 @@ abstract class FirehoseContract(http: HttpHandler) : AwsContract() {
     }
 
     @Test
+    @Disabled
     fun `send records`() {
         with(firehose) {
             assertThat(
