4a4f7db4e7e029d63937b35d14d55865564f5493
David Denton <denton.david@gmail.com>
2021-09-27 19:53:38 +0100

    remove comment

--- amazon/sqs/client/src/main/kotlin/org/http4k/connect/amazon/sqs/action/ReceiveMessage.kt
+++ amazon/sqs/client/src/main/kotlin/org/http4k/connect/amazon/sqs/action/ReceiveMessage.kt
@@ -18,8 +18,6 @@ import java.time.Duration
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME
 
-// can be QueueUrl
-
 @Http4kConnectAction
 data class ReceiveMessage(
     val queueUrl: Uri,
