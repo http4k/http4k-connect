16c77db183734b1725ab0c6a9cbbee4ebd83a925
David Denton <denton.david@gmail.com>
2021-11-16 09:03:08 +0000

    fix base64 decoding

--- amazon/core/src/main/kotlin/org/http4k/connect/amazon/core/model/Base64Blob.kt
+++ amazon/core/src/main/kotlin/org/http4k/connect/amazon/core/model/Base64Blob.kt
@@ -3,13 +3,14 @@ package org.http4k.connect.amazon.core.model
 import dev.forkhandles.values.NonBlankStringValueFactory
 import dev.forkhandles.values.StringValue
 import org.http4k.base64Decoded
+import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.util.Base64
 
 class Base64Blob private constructor(value: String) : StringValue(value) {
     fun decoded() = value.base64Decoded()
-    fun decodedBytes() = value.base64Decoded().toByteArray()
-    fun decodedInputStream() = value.base64Decoded().byteInputStream()
+    fun decodedBytes(): ByteArray = Base64.getDecoder().decode(value)
+    fun decodedInputStream() = ByteArrayInputStream(decodedBytes())
 
     companion object : NonBlankStringValueFactory<Base64Blob>(::Base64Blob) {
         fun encode(unencoded: String) = encode(unencoded.toByteArray())
--- amazon/core/src/test/kotlin/org/http4k/connect/amazon/core/model/Base64BlobTest.kt
+++ amazon/core/src/test/kotlin/org/http4k/connect/amazon/core/model/Base64BlobTest.kt
@@ -2,7 +2,10 @@ package org.http4k.connect.amazon.core.model
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import junit.framework.Assert.assertTrue
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import kotlin.random.Random
 
 class Base64BlobTest {
 
@@ -23,11 +26,19 @@ class Base64BlobTest {
     }
 
     @Test
+    fun `encode decode array`() {
+        val message = Random(0).nextBytes(50)
+        val encoded = Base64Blob.encode(message)
+        assertThat(encoded.value, equalTo("LMK0jFCu/lOzl07ZHmtOqST5uqjne8wvU38LAu/oYDCsLDFTRqj13MCnlgL5pRMQ2Yg="))
+        assertTrue(encoded.decodedBytes().contentEquals(message))
+    }
+
+    @Test
     fun `encode decode stream`() {
-        val message = "hello"
-        val stream = message.byteInputStream()
+        val message = Random(0).nextBytes(50)
+        val stream = ByteArrayInputStream(message)
         val encoded = Base64Blob.encode(stream)
-        assertThat(encoded.value, equalTo("aGVsbG8="))
-        assertThat(String(encoded.decodedInputStream().readAllBytes()), equalTo(message))
+        assertThat(encoded.value, equalTo("LMK0jFCu/lOzl07ZHmtOqST5uqjne8wvU38LAu/oYDCsLDFTRqj13MCnlgL5pRMQ2Yg="))
+        assertTrue(encoded.decodedInputStream().readAllBytes().contentEquals(message))
     }
 }
