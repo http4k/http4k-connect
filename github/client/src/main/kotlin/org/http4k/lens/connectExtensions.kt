05278d493ab09e1ba7e18bafe9a9965f56e7f169
David Denton <denton.david@gmail.com>
2022-03-13 19:29:51 +0000

    made github headers required for webhooks

--- github/client/src/main/kotlin/org/http4k/lens/connectExtensions.kt
+++ github/client/src/main/kotlin/org/http4k/lens/connectExtensions.kt
@@ -4,11 +4,11 @@ import org.http4k.connect.github.callback.CallbackEvent
 import org.http4k.core.ContentType
 import org.http4k.core.HttpMessage
 
-val Header.X_GITHUB_DELIVERY get() = Header.uuid().optional("X-GitHub-Delivery")
-val Header.X_GITHUB_EVENT get() = Header.enum<HttpMessage, CallbackEvent>().optional("X-GitHub-Event")
+val Header.X_GITHUB_DELIVERY get() = Header.uuid().required("X-GitHub-Delivery")
+val Header.X_GITHUB_EVENT get() = Header.enum<HttpMessage, CallbackEvent>().required("X-GitHub-Event")
 val Header.X_HUB_SIGNATURE_256
     get() = Header
         .map({ it.split("sha256=")[1] }, { "sha256=$it" })
-        .optional("X-Hub-Signature-256")
+        .required("X-Hub-Signature-256")
 
 val ContentType.Companion.GITHUB_JSON get() = ContentType("application/vnd.github+json")
