12c917149913f35c8498efaf4c26f5b245e1dd85
David Denton <denton.david@gmail.com>
2022-05-16 21:59:32 +0100

    add local dynamo

--- amazon/dynamodb/fake/build.gradle.kts
+++ amazon/dynamodb/fake/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("dev.forkhandles:parser4k")
+    implementation("com.amazonaws:DynamoDBLocal:_")
     testImplementation(project(path = ":http4k-connect-amazon-core", configuration = "testArtifacts"))
 }
