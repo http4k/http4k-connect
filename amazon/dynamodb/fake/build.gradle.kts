54a30e0018ba559ac05bf1a07da5a08568a473ba
David Denton <denton.david@gmail.com>
2022-06-06 21:45:28 +0100

    Revert "add local dynamo"
    This reverts commit 12c917149913f35c8498efaf4c26f5b245e1dd85.

--- amazon/dynamodb/fake/build.gradle.kts
+++ amazon/dynamodb/fake/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     implementation("dev.forkhandles:parser4k")
-    implementation("com.amazonaws:DynamoDBLocal:_")
     testImplementation(project(path = ":http4k-connect-amazon-core", configuration = "testArtifacts"))
 }
