7b1623d2c9de4a026d4be230217af5ff67a9ecfe
David Denton <denton.david@gmail.com>
2022-06-12 15:57:44 +0100

    add automarshalling for pagination

--- core/client/src/main/kotlin/org/http4k/connect/Paged.kt
+++ core/client/src/main/kotlin/org/http4k/connect/Paged.kt
@@ -1,7 +1,13 @@
 package org.http4k.connect
 
+import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Success
 import dev.forkhandles.result4k.map
+import org.http4k.core.Response
+import org.http4k.format.AutoMarshalling
+import org.http4k.lens.LensExtractor
+import kotlin.reflect.KClass
 
 /**
  * Represents a Paged response
@@ -38,4 +44,39 @@ fun <Token, ItemType, Action : PagedAction<Token, ItemType, Rsp, Action>, Rsp : 
                 }
             }
     }
+}
+
+/**
+ * Superclass for paged actions where the response can be automarshalled
+ */
+abstract class AutomarshalledPagedAction<Token, ItemType : Any, PageType : Paged<Token, ItemType>>(
+    private val toResult: (List<ItemType>, Token?) -> PageType,
+    private val autoMarshalling: AutoMarshalling,
+    private val kClass: KClass<PageType>
+) : PagedAction<Token, ItemType, PageType, AutomarshalledPagedAction<Token, ItemType, PageType>>,
+    LensExtractor<Response, Token?> {
+
+    override fun toResult(response: Response) = with(response) {
+        when {
+            status.successful -> Success(
+                toResult(
+                    autoMarshalling
+                        .asA(
+                            """{"items":${bodyString()}}""".trimIndent(),
+                            kClass
+                        ).items,
+                    this@AutomarshalledPagedAction(this)
+                )
+            )
+            else -> Failure(RemoteFailure(toRequest().method, toRequest().uri, status, bodyString()))
+        }
+    }
+
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        javaClass != other?.javaClass -> false
+        else -> toRequest() == (other as Action<*>).toRequest()
+    }
+
+    override fun hashCode() = toRequest().hashCode()
 }
