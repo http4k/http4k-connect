f00a9a09b332d925aed6aa7a4ee095e22b2d74ec
David Denton <denton.david@gmail.com>
2022-09-14 16:01:27 +0100

    rerelease

--- purge-projects.sh
+++ purge-projects.sh
@@ -6,9 +6,10 @@ VERSION=$1
 
 for ARTIFACT in `cat projects.txt`
 do
-    curl -X PURGE https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom > /dev/null 2>&1
+    URL=https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom
+    curl -X PURGE $URL > /dev/null 2>&1
 
-    echo Purging $ARTIFACT $VERSION `curl -sq --head https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom | head -1`
+    echo Purging $ARTIFACT $VERSION `curl -sq --head $URL | head -1`
 done
 
 rm projects.txt
