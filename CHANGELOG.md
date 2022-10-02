f36eff3f1fee3b8a073a8c2ebeb69326777fd768
David Denton <denton.david@gmail.com>
2022-10-02 15:27:26 +0100

    added some extension methods to dynamo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 This list is not intended to be all-encompassing - it will document major and breaking API 
 changes with their rationale when appropriate. Given version `A.B.C.D`, breaking changes are to be expected in version number increments where changes in the `A` or `B` sections:
 
+### v3.22.2.0
+- **http4k-connect-*** - Upgrade dependencies.
+- **http4k-connect-amazon-dynamo*** - Added some useful methods for mapping Attributes such as `Attribute.map(BiDiMapping)` and `Attribute.list(BiDiMapping)`
+
 ### v3.22.1.0
 - **http4k-connect-*** - Upgrade dependencies.
 
--- amazon/dynamodb/client/src/main/kotlin/org/http4k/connect/amazon/dynamodb/model/extensions.kt
+++ amazon/dynamodb/client/src/main/kotlin/org/http4k/connect/amazon/dynamodb/model/extensions.kt
@@ -1,6 +1,7 @@
 package org.http4k.connect.amazon.dynamodb.model
 
 import org.http4k.connect.amazon.dynamodb.model.DynamoDataType.valueOf
+import org.http4k.lens.BiDiMapping
 
 typealias TokensToNames = Map<String, AttributeName>
 typealias TokensToValues = Map<String, AttributeValue>
@@ -22,4 +23,25 @@ fun <T> Attribute<T>.asKeySchema(keyType: KeyType) = KeySchema(name, keyType)
  * Used for creating tables
  */
 fun <T> Attribute<T>.asAttributeDefinition() = AttributeDefinition(name, dataType)
+
+/**
+ * Map items out of a collection
+ */
+fun <T> Attribute.AttrLensSpec<List<AttributeValue>>.map(next: BiDiMapping<Item, T>): Attribute.AttrLensSpec<List<T>> = map(
+    { it.mapNotNull { it.M }.map { next(it) } },
+    { it.map { AttributeValue.Map(next(it)) } },
+)
+
+/**
+ * Map items out of a collection
+ */
+fun <T> Attribute.Companion.list(next: BiDiMapping<Item, T>) = list().map(
+    { it.mapNotNull { it.M }.map { next(it) } },
+    { it.map { AttributeValue.Map(next(it)) } },
+)
+
+/**
+ * Map items out of a collection
+ */
+fun <T> Attribute.Companion.map(next: BiDiMapping<Item, T>) = map().map(next)
 
