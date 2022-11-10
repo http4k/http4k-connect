73eac4bdc788d7901973c9b368272e703c3f8080
albertlatacz <albert.latacz@gmail.com>
2022-11-10 19:06:50 +0000

    Collapsed constants.kt

--- google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
+++ google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
@@ -21,3 +21,6 @@ data class PageView(
     override val clientId: ClientId,
     override val userAgent: String = DEFAULT_USER_AGENT
 ) : Analytics()
+
+const val DEFAULT_USER_AGENT =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
--- google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/constants.kt
+++
@@ -1,4 +0,0 @@
-package org.http4k.connect.google.analytics.model
-
-const val DEFAULT_USER_AGENT =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
