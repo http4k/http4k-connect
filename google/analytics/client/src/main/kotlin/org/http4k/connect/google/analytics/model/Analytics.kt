8edc435534e3da7e4fe5c23d09c7c46a6cc9cfdf
albertlatacz <albert.latacz@gmail.com>
2022-11-10 19:40:32 +0000

    move lines

--- google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
+++ google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
@@ -3,8 +3,6 @@ package org.http4k.connect.google.analytics.model
 import dev.forkhandles.result4k.Result
 import org.http4k.connect.RemoteFailure
 
-typealias AnalyticsCollector = (Analytics) -> Result<Unit, RemoteFailure>
-
 sealed class Analytics {
     abstract val clientId: ClientId
     abstract val userAgent: String
@@ -26,6 +24,8 @@ data class PageView(
     override val clientId: ClientId,
     override val userAgent: String = DEFAULT_USER_AGENT
 ) : Analytics()
+
+typealias AnalyticsCollector = (Analytics) -> Result<Unit, RemoteFailure>
 
 const val DEFAULT_USER_AGENT =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
