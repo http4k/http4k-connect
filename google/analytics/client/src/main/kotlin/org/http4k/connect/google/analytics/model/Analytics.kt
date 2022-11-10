f10670a9570a8ba0ff495ab88332b723813cb8e2
albertlatacz <albert.latacz@gmail.com>
2022-11-10 19:44:06 +0000

    extracted GA url

--- google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
+++ google/analytics/client/src/main/kotlin/org/http4k/connect/google/analytics/model/Analytics.kt
@@ -2,6 +2,7 @@ package org.http4k.connect.google.analytics.model
 
 import dev.forkhandles.result4k.Result
 import org.http4k.connect.RemoteFailure
+import org.http4k.core.Uri
 
 sealed class Analytics {
     abstract val clientId: ClientId
@@ -27,5 +28,5 @@ data class PageView(
 
 typealias AnalyticsCollector = (Analytics) -> Result<Unit, RemoteFailure>
 
-const val DEFAULT_USER_AGENT =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
+val GOOGLE_ANALYTICS_URL = Uri.of("https://www.google-analytics.com")
+const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
