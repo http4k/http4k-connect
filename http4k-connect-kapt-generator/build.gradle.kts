718ebea276136960553b47465f5be3500b9d35bc
David Denton <denton.david@gmail.com>
2023-01-14 12:09:20 +0000

    upgrade versions

--- http4k-connect-kapt-generator/build.gradle.kts
+++ http4k-connect-kapt-generator/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(Libs.http4k_format_moshi)
     implementation(Libs.kotlinpoet)
+    implementation(libs.symbol.processing.api)
     implementation(Libs.kotlinpoet_metadata)
 }
