e62cf41e76e27866c0faa86ea460a64dce08a7e0
David Denton <denton.david@gmail.com>
2023-01-15 11:47:28 +0000

    finish off ksp version

--- http4k-connect-ksp-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectActionVisitor.kt
+++ http4k-connect-ksp-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectActionVisitor.kt
@@ -5,6 +5,7 @@ import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.google.devtools.ksp.symbol.KSNode
 import com.google.devtools.ksp.visitor.KSEmptyVisitor
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterSpec
@@ -16,68 +17,68 @@ import java.util.Locale
 class Http4kConnectActionVisitor(private val log: (Any?) -> Unit) :
     KSEmptyVisitor<KSClassDeclaration, List<FunSpec>>() {
     override fun visitClassDeclaration(
-        actionClass: KSClassDeclaration,
-        adapterClazz: KSClassDeclaration
+        classDeclaration: KSClassDeclaration,
+        data: KSClassDeclaration
     ): List<FunSpec> {
-        log("Processing " + actionClass.asStarProjectedType().declaration.qualifiedName!!.asString())
+        log("Processing " + classDeclaration.asStarProjectedType().declaration.qualifiedName!!.asString())
 
-        val ctr = actionClass.primaryConstructor!!
+        val ctr = classDeclaration.primaryConstructor!!
 
         return listOfNotNull(
-            generateActionExtension(actionClass, adapterClazz, ctr),
-            actionClass.takeIf {
+            generateActionExtension(classDeclaration, data, ctr),
+            classDeclaration.takeIf {
                 it.getAllSuperTypes().map { it.toClassName().canonicalName }
                     .contains(PagedAction::class.qualifiedName)
             }
-                ?.let { generateActionPagination(actionClass, adapterClazz, ctr) }
+                ?.let { generateActionPagination(classDeclaration, data, ctr) }
         )
     }
 
-    private fun generateActionPagination(
-        actionClass: KSClassDeclaration,
-        adapterClazz: KSClassDeclaration,
-        ctr: KSFunctionDeclaration
-    ) = generateExtensionFunction(
-        actionClass, adapterClazz, ctr, "Paginated", CodeBlock.of(
-            "return org.http4k.connect.paginated(::invoke, %T(${ctr.parameters.joinToString(", ") { it.name!!.asString() }}))",
-            actionClass.asType(emptyList()).toTypeName()
-        )
-    )
-
-    private fun generateActionExtension(
-        actionClass: KSClassDeclaration,
-        adapterClazz: KSClassDeclaration,
-        ctr: KSFunctionDeclaration
-    ) = generateExtensionFunction(
-        actionClass, adapterClazz, ctr, "", CodeBlock.of(
-            "return invoke(%T(${ctr.parameters.joinToString(", ") { it.name!!.asString() }}))",
-            actionClass.asType(emptyList()).toTypeName()
-        )
-    )
-
-    override fun defaultHandler(node: KSNode, data: KSClassDeclaration): List<FunSpec> {
-        error("unsupported")
-    }
-
-    private fun generateExtensionFunction(
-        actionClazz: KSClassDeclaration,
-        adapterClazz: KSClassDeclaration,
-        ctr: KSFunctionDeclaration,
-        suffix: String,
-        codeBlock: CodeBlock
-    ): FunSpec {
-        val baseFunction = FunSpec.builder(
-            actionClazz.simpleName.asString().replaceFirstChar { it.lowercase(Locale.getDefault()) } + suffix)
-            .addKdoc("@see ${actionClazz.simpleName.asString().replace('/', '.')}")
-            .receiver(adapterClazz.simpleName.asString().asClassName())
-            .addCode(codeBlock)
-
-        ctr.parameters.forEach {
-            val base = ParameterSpec.builder(it.name!!.asString(), it.type.toTypeName())
-            if (it.type.resolve().isMarkedNullable) base.defaultValue(CodeBlock.of("null"))
-            baseFunction.addParameter(base.build())
-        }
-        return baseFunction.build()
-    }
-
+    override fun defaultHandler(node: KSNode, data: KSClassDeclaration) = error("unsupported")
 }
+
+private fun generateActionPagination(
+    actionClass: KSClassDeclaration,
+    adapterClazz: KSClassDeclaration,
+    ctr: KSFunctionDeclaration
+) = generateExtensionFunction(
+    actionClass, adapterClazz, ctr, "Paginated", CodeBlock.of(
+        "return org.http4k.connect.paginated(::invoke, %T(${ctr.parameters.joinToString(", ") { it.name!!.asString() }}))",
+        actionClass.asType(emptyList()).toTypeName()
+    )
+)
+
+private fun generateActionExtension(
+    actionClass: KSClassDeclaration,
+    adapterClazz: KSClassDeclaration,
+    ctr: KSFunctionDeclaration
+) = generateExtensionFunction(
+    actionClass, adapterClazz, ctr, "", CodeBlock.of(
+        "return invoke(%T(${ctr.parameters.joinToString(", ") { it.name!!.asString() }}))",
+        actionClass.asType(emptyList()).toTypeName()
+    )
+)
+
+private fun generateExtensionFunction(
+    actionClazz: KSClassDeclaration,
+    adapterClazz: KSClassDeclaration,
+    ctr: KSFunctionDeclaration,
+    suffix: String,
+    codeBlock: CodeBlock
+): FunSpec {
+    val baseFunction = FunSpec.builder(
+        actionClazz.simpleName.asString().replaceFirstChar { it.lowercase(Locale.getDefault()) } + suffix)
+        .addKdoc("@see ${actionClazz.qualifiedName!!.asString().replace('/', '.')}")
+        .receiver(adapterClazz.simpleName.asString().asClassName())
+        .addCode(codeBlock)
+
+    ctr.parameters.forEach {
+        val base = ParameterSpec.builder(it.name!!.asString(), it.type.toTypeName())
+        if (it.type.resolve().isMarkedNullable) base.defaultValue(CodeBlock.of("null"))
+        baseFunction.addParameter(base.build())
+    }
+    return baseFunction.build()
+}
+internal fun kotlinx.metadata.ClassName.pkg() = substringBeforeLast("/").replace('/', '.')
+internal fun kotlinx.metadata.ClassName.name() = substringAfterLast('/')
+internal fun kotlinx.metadata.ClassName.asClassName() = ClassName(pkg(), name())
--- http4k-connect-ksp-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterVisitor.kt
+++ http4k-connect-ksp-generator/src/main/kotlin/org/http4k/connect/plugin/Http4kConnectAdapterVisitor.kt
@@ -10,28 +10,26 @@ import com.squareup.kotlinpoet.FileSpec
 import java.util.Locale
 
 class Http4kConnectAdapterVisitor(private val log: (Any?) -> Unit) : KSEmptyVisitor<List<KSAnnotated>, FileSpec>() {
-    override fun visitClassDeclaration(adapterClass: KSClassDeclaration, actions: List<KSAnnotated>): FileSpec {
+    override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: List<KSAnnotated>): FileSpec {
         log(
-            "Processing http4k-connect adapter: " + adapterClass.simpleName!!.asString() +
-                " with action type: ${adapterClass.http4kConnectActionType}"
+            "Processing http4k-connect adapter: " + classDeclaration.simpleName.asString() +
+                " with action type: ${classDeclaration.http4kConnectActionType}"
         )
 
         return FileSpec.builder(
-            adapterClass.packageName.asString(),
-            adapterClass.simpleName.asString().lowercase(Locale.getDefault()) + "Extensions"
+            classDeclaration.packageName.asString(),
+            classDeclaration.simpleName.asString().lowercase(Locale.getDefault()) + "Extensions"
         )
             .apply {
-                actions.filterForActionsOf(adapterClass)
-                    .flatMap { it.accept(Http4kConnectActionVisitor(log), adapterClass) }
+                data.filterForActionsOf(classDeclaration)
+                    .flatMap { it.accept(Http4kConnectActionVisitor(log), classDeclaration) }
                     .forEach(::addFunction)
             }
             .build()
 
     }
 
-    override fun defaultHandler(node: KSNode, data: List<KSAnnotated>): FileSpec {
-        error("unsupported")
-    }
+    override fun defaultHandler(node: KSNode, data: List<KSAnnotated>) = error("unsupported")
 }
 
 private val KSClassDeclaration.http4kConnectActionType
