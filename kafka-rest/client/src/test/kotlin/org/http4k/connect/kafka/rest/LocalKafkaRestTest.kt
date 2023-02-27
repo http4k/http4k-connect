64d557ce27fae97269bc869a6e38c3e2e493d9b4
David <denton.david@gmail.com>
2023-02-27 23:13:20 +0000

    remove debug

--- kafka-rest/client/src/test/kotlin/org/http4k/connect/kafka/rest/LocalKafkaRestTest.kt
+++ kafka-rest/client/src/test/kotlin/org/http4k/connect/kafka/rest/LocalKafkaRestTest.kt
@@ -3,16 +3,17 @@ package org.http4k.connect.kafka.rest
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.assumeDockerDaemonRunning
 import org.http4k.core.Uri
+import org.junit.jupiter.api.Disabled
 
 class LocalKafkaRestTest : KafkaRestContract() {
     init {
         assumeDockerDaemonRunning()
     }
 
-//    @Disabled("no schema registry available in compose")
-//    override fun `can send AVRO messages and get them back`() {
-//        super.`can send AVRO messages and get them back`()
-//    }
+    @Disabled("no schema registry available in compose")
+    override fun `can send AVRO messages and get them back`() {
+        super.`can send AVRO messages and get them back`()
+    }
 
     override val uri = Uri.of("http://localhost:38082")
 
