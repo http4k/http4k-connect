2d07cdfc0ed2cec6dbc9e270ae147a6a17d364ae
David <denton.david@gmail.com>
2023-04-10 15:43:40 +0100

    add image generation test to openapi

--- openai/client/src/test/kotlin/org/http4k/connect/openai/OpenAIContract.kt
+++ openai/client/src/test/kotlin/org/http4k/connect/openai/OpenAIContract.kt
@@ -7,9 +7,14 @@ import org.http4k.connect.openai.OpenAIOrg.Companion.OPENAI
 import org.http4k.connect.openai.Role.Companion.System
 import org.http4k.connect.openai.Role.Companion.User
 import org.http4k.connect.openai.action.Message
+import org.http4k.connect.openai.action.Size
 import org.http4k.connect.successValue
+import org.http4k.testing.ApprovalTest
+import org.http4k.testing.Approver
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(ApprovalTest::class)
 interface OpenAIContract {
 
     val openAi: OpenAI
@@ -35,5 +40,12 @@ interface OpenAIContract {
             ).successValue().model,
             equalTo(GPT3_5)
         )
+    }
+
+
+    @Test
+    fun `can generate image`(approver: Approver) {
+        val generated = openAi.generateImage(Content.of("An excellent library"), Size.`256x256`).successValue()
+        println(generated.data.first().url)
     }
 }
--- openai/fake/src/test/kotlin/org/http4k/connect/openai/FakeOpenAITest.kt
+++ openai/fake/src/test/kotlin/org/http4k/connect/openai/FakeOpenAITest.kt
@@ -2,25 +2,23 @@ package org.http4k.connect.openai
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.connect.openai.action.ImageResponseFormat.*
+import org.http4k.connect.openai.action.ImageResponseFormat.b64_json
+import org.http4k.connect.openai.action.ImageResponseFormat.url
 import org.http4k.connect.openai.action.Size
 import org.http4k.connect.successValue
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Uri
-import org.http4k.testing.ApprovalTest
 import org.http4k.testing.Approver
 import org.http4k.testing.assertApproved
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 
-@ExtendWith(ApprovalTest::class)
 class FakeOpenAITest : OpenAIContract {
     private val fakeOpenAI = FakeOpenAI()
     override val openAi = OpenAI.Http(OpenAIToken.of("hello"), fakeOpenAI)
 
     @Test
-    fun `can generate and server image from url`(approver: Approver) {
+    fun `can generate and serve image from url`(approver: Approver) {
         val generated = openAi.generateImage(
             Content.of("An excellent library"), Size.`1024x1024`,
             url, 1.0, null
@@ -32,7 +30,7 @@ class FakeOpenAITest : OpenAIContract {
     }
 
     @Test
-    fun `can generate and server image as data url`(approver: Approver) {
+    fun `can generate and serve image as data url`(approver: Approver) {
         val generated = openAi.generateImage(
             Content.of("An excellent library"), Size.`1024x1024`,
             b64_json, 1.0, null
