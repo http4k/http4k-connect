e50f3b2169b06431217a844271dc70a3168544e7
David Denton <denton.david@gmail.com>
2023-05-04 18:56:19 +0100

    fix deprecation import

--- github/client/src/main/kotlin/org/http4k/connect/github/api/action/deprecated.kt
+++ github/client/src/main/kotlin/org/http4k/connect/github/api/action/deprecated.kt
@@ -3,5 +3,5 @@ package org.http4k.connect.github.api.action
 @Deprecated("Moved", ReplaceWith("org.http4k.connect.github.api.GitHubWebhookAction"))
 typealias GitHubWebhookAction = org.http4k.connect.github.api.GitHubWebhookAction
 
-@Deprecated("Moved", ReplaceWith("org.http4k.connect.example.GitHubAction<T>"))
+@Deprecated("Moved", ReplaceWith("org.http4k.connect.github.api.GitHubAction<T>"))
 typealias GitHubAction<T> = org.http4k.connect.github.api.GitHubAction<T>
