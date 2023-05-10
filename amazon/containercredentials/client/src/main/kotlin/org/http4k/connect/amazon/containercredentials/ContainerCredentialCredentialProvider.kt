884b8b9ce93fd03769b790e810c2d45405722ab6
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-05-10 14:12:53 -0400

    Add CredentialsChain for Amazon Containers

--- amazon/containercredentials/client/src/main/kotlin/org/http4k/connect/amazon/containercredentials/ContainerCredentialCredentialProvider.kt
+++ amazon/containercredentials/client/src/main/kotlin/org/http4k/connect/amazon/containercredentials/ContainerCredentialCredentialProvider.kt
@@ -1,10 +1,15 @@
 package org.http4k.connect.amazon.containercredentials
 
-import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.map
+import dev.forkhandles.result4k.onFailure
+import dev.forkhandles.result4k.peek
+import dev.forkhandles.result4k.valueOrNull
 import org.http4k.client.JavaHttpClient
 import org.http4k.cloudnative.env.Environment
 import org.http4k.connect.RemoteFailure
+import org.http4k.connect.amazon.CredentialsChain
 import org.http4k.connect.amazon.CredentialsProvider
 import org.http4k.connect.amazon.containercredentials.action.getCredentials
 import org.http4k.connect.amazon.core.model.Credentials
@@ -14,6 +19,60 @@ import java.time.Clock
 import java.time.Duration
 import java.util.concurrent.atomic.AtomicReference
 
+private fun credentialProvider(
+    containerCredentials: ContainerCredentials,
+    uri: Uri,
+    clock: Clock,
+    gracePeriod: Duration
+): () -> Result4k<Credentials, RemoteFailure> {
+    val credentials = AtomicReference<Credentials>(null)
+
+    fun refresh(): Result4k<Credentials, RemoteFailure> =
+        synchronized(credentials) {
+            val current = credentials.get()
+            when {
+                current != null && !current.expiresWithin(clock, gracePeriod) -> Success(current)
+                else -> containerCredentials
+                    .getCredentials(uri)
+                    .peek { credentials.set(it) }
+            }
+        }
+
+    return {
+        credentials.get()
+            ?.takeUnless { it.expiresWithin(clock, gracePeriod) }
+            ?.let { Success(it) }
+            ?: refresh()
+    }
+}
+
+/**
+ * Refreshing credentials provider for getting credentials from the container credentials service .
+ */
+fun CredentialsChain.Companion.ContainerCredentials(
+    containerCredentials: ContainerCredentials,
+    uri: Uri,
+    clock: Clock,
+    gracePeriod: Duration
+): CredentialsChain {
+    val provider = credentialProvider(containerCredentials, uri, clock, gracePeriod)
+
+    return CredentialsChain {
+        provider().map { it.asHttp4k() }.valueOrNull()
+    }
+}
+
+fun CredentialsChain.Companion.ContainerCredentials(
+    env: Environment,
+    http: HttpHandler = JavaHttpClient(),
+    clock: Clock = Clock.systemUTC(),
+    gracePeriod: Duration = Duration.ofSeconds(300)
+) = CredentialsChain.ContainerCredentials(
+    ContainerCredentials.Http(http, AWS_CONTAINER_AUTHORIZATION_TOKEN(env)),
+    AWS_CONTAINER_CREDENTIALS_FULL_URI(env),
+    clock, gracePeriod
+)
+
 /**
  * Refreshing credentials provider for getting credentials from the container credentials service .
  */
@@ -22,28 +81,12 @@ fun CredentialsProvider.Companion.ContainerCredentials(
     uri: Uri,
     clock: Clock,
     gracePeriod: Duration
-) = object : CredentialsProvider {
+): CredentialsProvider {
+    val provider = credentialProvider(containerCredentials, uri, clock, gracePeriod)
 
-    private val credentials = AtomicReference<Credentials>(null)
-
-    override fun invoke() =
-        (credentials.get()?.takeIf { !it.expiresWithin(clock, gracePeriod) } ?: refresh()).asHttp4k()
-
-    private fun refresh() =
-        synchronized(credentials) {
-            val current = credentials.get()
-            when {
-                current != null && !current.expiresWithin(clock, gracePeriod) -> current
-                else -> when (val refreshed = containerCredentials.getCredentials(uri)) {
-                    is Success<Credentials> -> {
-                        val newCreds = refreshed.value
-                        credentials.set(newCreds)
-                        newCreds
-                    }
-                    is Failure<RemoteFailure> -> refreshed.reason.throwIt()
-                }
-            }
-        }
+    return CredentialsProvider {
+        provider().map { it.asHttp4k() }.onFailure { it.reason.throwIt() }
+    }
 }
 
 fun CredentialsProvider.Companion.ContainerCredentials(
