d3197fcf24659fde20915f720d20734267652317
David Denton <denton.david@gmail.com>
2023-05-28 15:17:20 +0100

    consolidated test harnesses

--- openai/plugin/src/examples/kotlin/addressbook/env/startOAuthPlugin.kt
+++ openai/plugin/src/examples/kotlin/addressbook/env/startOAuthPlugin.kt
@@ -1,6 +1,6 @@
-package addressbook.oauth
+package addressbook.env
 
-
+import addressbook.oauth.OAuthPlugin
 import addressbook.oauth.OAuthPluginSettings.COOKIE_DOMAIN
 import addressbook.oauth.OAuthPluginSettings.EMAIL
 import addressbook.oauth.OAuthPluginSettings.OPENAI_CLIENT_CREDENTIALS
@@ -9,14 +9,13 @@ import addressbook.oauth.OAuthPluginSettings.OPENAI_VERIFICATION_TOKEN
 import addressbook.oauth.OAuthPluginSettings.PLUGIN_BASE_URL
 import addressbook.oauth.OAuthPluginSettings.PORT
 import addressbook.oauth.OAuthPluginSettings.REDIRECTION_URLS
-import org.http4k.chaos.start
 import org.http4k.cloudnative.env.Environment.Companion.ENV
-import org.http4k.connect.openai.FakeOpenAI
 import org.http4k.connect.openai.auth.OpenAIPluginId
 import org.http4k.connect.openai.auth.oauth.openaiPlugin
 import org.http4k.connect.openai.model.Email
 import org.http4k.connect.openai.model.VerificationToken
 import org.http4k.connect.openai.plugins.OAuthPluginIntegrationBuilder
+import org.http4k.connect.openai.plugins.PluginIntegrationBuilder
 import org.http4k.core.Credentials
 import org.http4k.core.Uri
 import org.http4k.core.then
@@ -27,33 +26,19 @@ import org.http4k.security.OAuthProvider
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
-/**
- * This program runs a Plugin and Fake OpenAI server. Run it and browse to the local port
- * to demonstrate the login flow.
- */
-fun main() {
-    val commonEnv = ENV.with(
-        OPENAI_PLUGIN_ID of OpenAIPluginId.of("myplugin"),
-        PLUGIN_BASE_URL of Uri.of("http://localhost:8000"),
+fun startOAuthPlugin(openAiPort: Int): PluginIntegrationBuilder {
+    val pluginId = OpenAIPluginId.of("oauthplugin")
+
+    val env = ENV.with(
+        PORT of 10000,
+        OPENAI_PLUGIN_ID of pluginId,
+        PLUGIN_BASE_URL of Uri.of("http://localhost:10000"),
         OPENAI_CLIENT_CREDENTIALS of Credentials("foo", "bar"),
-    )
-
-    val fakeOpenAi = FakeOpenAI(
-        plugins = arrayOf(
-            OAuthPluginIntegrationBuilder(
-                OPENAI_PLUGIN_ID(commonEnv),
-                OAuthProvider.openaiPlugin(PLUGIN_BASE_URL(commonEnv), OPENAI_CLIENT_CREDENTIALS(commonEnv)),
-            )
-        )
-    ).start()
-
-    val env = commonEnv.with(
-        PORT of 8000,
         EMAIL of Email.of("foo@bar.com"),
         COOKIE_DOMAIN of "localhost",
         OPENAI_VERIFICATION_TOKEN of VerificationToken.of("supersecret"),
         REDIRECTION_URLS of listOf(
-            Uri.of("http://localhost:${fakeOpenAi.port()}/aip/plugin-${OPENAI_PLUGIN_ID(commonEnv)}/oauth/callback")
+            Uri.of("http://localhost:$openAiPort/aip/plugin-${pluginId}/oauth/callback")
         )
     )
 
@@ -62,5 +47,11 @@ fun main() {
         .asServer(SunHttp(PORT(env)))
         .start()
 
-    println("Login to http://localhost:${fakeOpenAi.port()}")
+    return OAuthPluginIntegrationBuilder(
+        OPENAI_PLUGIN_ID(env),
+        OAuthProvider.openaiPlugin(
+            PLUGIN_BASE_URL(env),
+            OPENAI_CLIENT_CREDENTIALS(env)
+        ),
+    )
 }
--- openai/plugin/src/examples/kotlin/addressbook/env/startUserPlugin.kt
+++ openai/plugin/src/examples/kotlin/addressbook/env/startUserPlugin.kt
@@ -1,47 +1,29 @@
-package addressbook.user
-
+package addressbook.env
 
 import addressbook.shared.UserDirectory
+import addressbook.user.UserPlugin
 import addressbook.user.UserPluginSettings.EMAIL
 import addressbook.user.UserPluginSettings.PLUGIN_BASE_URL
 import addressbook.user.UserPluginSettings.PORT
-import org.http4k.chaos.start
 import org.http4k.cloudnative.env.Environment.Companion.ENV
-import org.http4k.connect.openai.FakeOpenAI
 import org.http4k.connect.openai.auth.OpenAIPluginId
 import org.http4k.connect.openai.model.Email
+import org.http4k.connect.openai.plugins.PluginIntegrationBuilder
 import org.http4k.connect.openai.plugins.UserPluginIntegrationBuilder
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.core.with
 import org.http4k.filter.CorsPolicy.Companion.UnsafeGlobalPermissive
-import org.http4k.filter.ServerFilters
+import org.http4k.filter.ServerFilters.BasicAuth
 import org.http4k.filter.ServerFilters.Cors
 import org.http4k.filter.debug
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
-/**
- * This program runs a Plugin and Fake OpenAI server. Run it and browse to the local port
- * to demonstrate the login flow.
- */
-fun main() {
-    val commonEnv = ENV.with(
-        PLUGIN_BASE_URL of Uri.of("http://localhost:8000")
-    )
-
-    val fakeOpenAi = FakeOpenAI(
-        plugins = arrayOf(
-            UserPluginIntegrationBuilder(
-                ServerFilters.BasicAuth("") { UserDirectory().auth(it) != null },
-                OpenAIPluginId.of("userplugin"),
-                PLUGIN_BASE_URL(commonEnv)
-            )
-        )
-    ).start()
-
-    val env = commonEnv.with(
-        PORT of 8000,
+fun startUserPlugin(): PluginIntegrationBuilder {
+    val env = ENV.with(
+        PORT of 30000,
+        PLUGIN_BASE_URL of Uri.of("http://localhost:30000"),
         EMAIL of Email.of("foo@bar.com"),
     )
 
@@ -51,5 +33,9 @@ fun main() {
         .asServer(SunHttp(PORT(env)))
         .start()
 
-    println("Login to http://localhost:${fakeOpenAi.port()}")
+    return UserPluginIntegrationBuilder(
+        BasicAuth("") { UserDirectory().auth(it) != null },
+        OpenAIPluginId.of("userplugin"),
+        PLUGIN_BASE_URL(env)
+    )
 }
