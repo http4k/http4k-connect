b53c91e9a7f221bff1d5e8a603d7e5f0f7be7e01
David Denton <denton.david@gmail.com>
2023-06-11 07:08:33 +0100

    tweaks to add value type extensions

--- storage/core/src/main/kotlin/org/http4k/connect/storage/Storage.kt
+++ storage/core/src/main/kotlin/org/http4k/connect/storage/Storage.kt
@@ -7,6 +7,9 @@ package org.http4k.connect.storage
 interface Storage<T : Any> {
     operator fun get(key: String): T?
     operator fun set(key: String, data: T)
+    operator fun minusAssign(key: String) {
+        remove(key)
+    }
     fun remove(key: String): Boolean
     fun keySet(keyPrefix: String = ""): Set<String>
     fun removeAll(keyPrefix: String = ""): Boolean
