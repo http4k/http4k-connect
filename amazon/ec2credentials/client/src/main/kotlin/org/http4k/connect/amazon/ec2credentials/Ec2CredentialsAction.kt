1d02a0dddd59904f97c36f2c8eecfd416a3bdf30
David Denton <denton.david@gmail.com>
2023-06-12 14:52:20 +0100

    remove ec2 metadata service (as was deprecated)

--- amazon/ec2credentials/client/src/main/kotlin/org/http4k/connect/amazon/ec2credentials/Ec2CredentialsAction.kt
+++
@@ -1,7 +0,0 @@
-package org.http4k.connect.amazon.ec2credentials
-
-import dev.forkhandles.result4k.Result
-import org.http4k.connect.Action
-import org.http4k.connect.RemoteFailure
-
-interface Ec2CredentialsAction<R> : Action<Result<R, RemoteFailure>>
