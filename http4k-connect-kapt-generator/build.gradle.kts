cb5487b45ecc7da6e6a2b3d0a9e82962d3001fd9
David Denton <denton.david@gmail.com>
2023-06-19 11:54:21 +0100

    removal of the http4k-connect-kapt-generator module as has been replaced with the http4k-connect-ksp-generator. This is a drop-in replacement and is consistent with the direction of travel in the Kotlin ecosystem.

--- http4k-connect-kapt-generator/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation(Libs.http4k_format_moshi)
-    implementation(Libs.kotlinpoet)
-    implementation(libs.symbol.processing.api)
-    implementation(Libs.kotlinpoet_metadata)
-}
