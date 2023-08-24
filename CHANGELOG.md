d6b1b7cc50bf264075e6fc8ca096e1c447db9124
David Denton <denton.david@gmail.com>
2023-08-24 18:24:47 +0100

    fix moshi delivery stream mapping

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 This list is not intended to be all-encompassing - it will document major and breaking API 
 changes with their rationale when appropriate. Given version `A.B.C.D`, breaking changes are to be expected in version number increments where changes in the `A` or `B` sections:
 
+### v5.1.6.1
+- **http4k-connect-*** - Upgrade dependencies.
+- **http4k-connect-amazon-firehose*** - [Fix] Serialize DeliveryStreamName correctly in Moshi.
+
 ### v5.1.6.0
 - **http4k-connect-*** - Upgrade dependencies.
 - **http4k-connect-**** - Make generation of IDs more deterministic
--- amazon/firehose/client/src/main/kotlin/org/http4k/connect/amazon/firehose/FirehoseMoshi.kt
+++ amazon/firehose/client/src/main/kotlin/org/http4k/connect/amazon/firehose/FirehoseMoshi.kt
@@ -2,11 +2,13 @@ package org.http4k.connect.amazon.firehose
 
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import org.http4k.connect.amazon.model.DeliveryStreamName
 import org.http4k.format.AwsCoreJsonAdapterFactory
 import org.http4k.format.ConfigurableMoshi
 import org.http4k.format.ListAdapter
 import org.http4k.format.MapAdapter
 import org.http4k.format.asConfigurable
+import org.http4k.format.value
 import org.http4k.format.withAwsCoreMappings
 import org.http4k.format.withStandardMappings
 import se.ansman.kotshi.KotshiJsonAdapterFactory
@@ -18,6 +20,7 @@ object FirehoseMoshi : ConfigurableMoshi(
         .add(ListAdapter)
         .add(MapAdapter)
         .asConfigurable()
+        .value(DeliveryStreamName)
         .withStandardMappings()
         .withAwsCoreMappings()
         .done()
