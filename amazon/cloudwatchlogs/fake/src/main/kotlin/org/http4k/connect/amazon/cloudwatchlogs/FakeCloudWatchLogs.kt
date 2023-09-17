610e5c10ad109e207d283392ce36d11252ddf4cd
David Denton <denton.david@gmail.com>
2023-09-17 12:43:07 +0100

    adding more cwl implementation

--- amazon/cloudwatchlogs/fake/src/main/kotlin/org/http4k/connect/amazon/cloudwatchlogs/FakeCloudWatchLogs.kt
+++ amazon/cloudwatchlogs/fake/src/main/kotlin/org/http4k/connect/amazon/cloudwatchlogs/FakeCloudWatchLogs.kt
@@ -5,14 +5,14 @@ import org.http4k.chaos.ChaoticHttpHandler
 import org.http4k.chaos.start
 import org.http4k.connect.amazon.AmazonJsonFake
 import org.http4k.connect.amazon.cloudwatchlogs.action.FilteredLogEvent
+import org.http4k.connect.amazon.cloudwatchlogs.model.LogStreamName
 import org.http4k.connect.amazon.core.model.AwsService
 import org.http4k.connect.amazon.core.model.Region
-import org.http4k.connect.amazon.cloudwatchlogs.model.LogStreamName
 import org.http4k.connect.storage.InMemory
 import org.http4k.connect.storage.Storage
 import org.http4k.core.Method.POST
 import org.http4k.routing.bind
-import org.http4k.aws.AwsCredentialsimport org.http4k.routing.routes
+import org.http4k.routing.routes
 
 data class LogGroup(val streams: MutableMap<LogStreamName, MutableList<FilteredLogEvent>>)
 
