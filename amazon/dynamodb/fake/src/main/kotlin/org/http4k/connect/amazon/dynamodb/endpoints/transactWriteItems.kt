1fffeb328a4205b1a9dbab5743899b53788672cd
David Denton <denton.david@gmail.com>
2023-09-24 10:30:05 +0100

    adding transactions to fake dynamo

--- amazon/dynamodb/fake/src/main/kotlin/org/http4k/connect/amazon/dynamodb/endpoints/transactWriteItems.kt
+++ amazon/dynamodb/fake/src/main/kotlin/org/http4k/connect/amazon/dynamodb/endpoints/transactWriteItems.kt
@@ -1,6 +1,8 @@
 package org.http4k.connect.amazon.dynamodb.endpoints
 
 import org.http4k.connect.amazon.AmazonJsonFake
+import org.http4k.connect.amazon.JsonError
+import org.http4k.connect.amazon.dynamodb.DynamoDbAction
 import org.http4k.connect.amazon.dynamodb.DynamoDbMoshi.convert
 import org.http4k.connect.amazon.dynamodb.DynamoTable
 import org.http4k.connect.amazon.dynamodb.action.DeleteItem
@@ -9,37 +11,58 @@ import org.http4k.connect.amazon.dynamodb.action.ModifiedItems
 import org.http4k.connect.amazon.dynamodb.action.PutItem
 import org.http4k.connect.amazon.dynamodb.action.TransactWriteItems
 import org.http4k.connect.amazon.dynamodb.action.UpdateItem
+import org.http4k.connect.amazon.dynamodb.endpoints.UpdateResult.UpdateOk
 import org.http4k.connect.storage.Storage
 
 fun AmazonJsonFake.transactWriteItems(tables: Storage<DynamoTable>) = route<TransactWriteItems> {
     synchronized(tables) {
-        it.TransactItems.forEach { write ->
-            write.Delete?.let {
-                val req = convert<Map<String, Any?>, DeleteItem>(it)
-                val tableName = req.TableName
-                tables[tableName.value]?.let { existing ->
-                    tables[tableName.value] = existing.withoutItem(req.Key)
+        val transactionItems = it.toTransactionItems()
+        when {
+            transactionItems.size != it.TransactItems.size -> JsonError("in tx", "some transactions bad")
+            else -> {
+                val attempts = transactionItems.attemptUsing(tables)
+                when {
+                    attempts.isNotEmpty() -> JsonError("in tx", attempts.map { it.toString() }.joinToString(","))
+                    else -> {
+                        transactionItems.applyTo(tables)
+                        ModifiedItems()
+                    }
                 }
             }
-            write.Put?.let {
-                val req = convert<Map<String, Any?>, PutItem>(it)
-                val table = tables[req.TableName.value] ?: return@route null
-                tables[req.TableName.value] = table.withItem(req.Item)
-                ModifiedItem(req.Item.asItemResult())
-            }
-            write.Update?.let {
-                val req = convert<Map<String, Any?>, UpdateItem>(it)
-                val table = tables[req.TableName.value] ?: return@route null
-                val existingItem = table.retrieve(req.Key) ?: req.Key
-
-                val updated = existingItem.update(
-                    expression = req.UpdateExpression,
-                    expressionAttributeNames = req.ExpressionAttributeNames,
-                    expressionAttributeValues = req.ExpressionAttributeValues
-                )
-                tables[req.TableName.value] = table.withoutItem(existingItem).withItem(updated)
-            }
         }
-        ModifiedItems()
     }
 }
+
+private fun List<DynamoDbAction<ModifiedItem>>.applyTo(tables: Storage<DynamoTable>) {
+    forEach {
+        when (it) {
+            is DeleteItem -> tables.runUpdate(it.TableName, it, tryModifyDelete)
+            is PutItem -> tables.runUpdate(it.TableName, it, tryModifyPut)
+            is UpdateItem -> tables.runUpdate(it.TableName, it, tryModifyUpdate)
+            else -> error("bug $it")
+        }
+    }
+}
+
+private fun List<DynamoDbAction<ModifiedItem>>.attemptUsing(tables: Storage<DynamoTable>) = mapNotNull {
+    val result = when (it) {
+        is DeleteItem -> tryModifyDelete(it, tables[it.TableName.value]!!)
+        is PutItem -> tryModifyPut(it, tables[it.TableName.value]!!)
+        is UpdateItem -> tryModifyUpdate(it, tables[it.TableName.value]!!)
+        else -> error("bug $it")
+    }
+    when (result) {
+        is UpdateOk -> null
+        else -> it
+    }
+}
+
+private fun TransactWriteItems.toTransactionItems() =
+    TransactItems.mapNotNull { write ->
+        when {
+            write.Delete != null -> convert<Map<String, Any?>, DeleteItem>(write.Delete!!)
+            write.Put != null -> convert<Map<String, Any?>, PutItem>(write.Put!!)
+            write.Update != null -> convert<Map<String, Any?>, UpdateItem>(write.Update!!)
+            else -> null
+        }
+    }
