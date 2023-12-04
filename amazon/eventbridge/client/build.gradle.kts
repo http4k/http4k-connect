5e50e8b946ed32dc651c921bc072c3882c3552ff
David Denton <denton.david@gmail.com>
2023-12-04 20:08:13 +0000

    upgrade dependencies. Add shim method for converting between EventBridge and Serverless version

--- amazon/eventbridge/client/build.gradle.kts
+++ amazon/eventbridge/client/build.gradle.kts
@@ -3,6 +3,9 @@ dependencies {
     api(Libs.http4k_format_moshi) {
         exclude("org.jetbrains.kotlin", "kotlin-reflect")
     }
+    compileOnly(Libs.http4k_serverless_lambda)
+    compileOnly("joda-time:joda-time:_")
+
     implementation(Libs.api)
 
     testImplementation(project(path = ":http4k-connect-core", configuration = "testArtifacts"))
--- amazon/eventbridge/client/src/main/kotlin/org/http4k/connect/amazon/eventbridge/model/Event.kt
+++ amazon/eventbridge/client/src/main/kotlin/org/http4k/connect/amazon/eventbridge/model/Event.kt
@@ -1,7 +1,9 @@
 package org.http4k.connect.amazon.eventbridge.model
 
+import com.amazonaws.services.lambda.runtime.events.EventBridgeEvent
 import org.http4k.connect.amazon.core.model.ARN
 import org.http4k.connect.amazon.core.model.Timestamp
+import org.http4k.connect.amazon.eventbridge.EventBridgeMoshi
 import org.http4k.connect.amazon.model.EventBusName
 import org.http4k.connect.amazon.model.EventDetail
 import org.http4k.connect.amazon.model.EventDetailType
@@ -18,3 +20,13 @@ data class Event(
     val Time: Timestamp? = null,
     val TraceHeader: String? = null
 )
+
+/**
+ * Shim method to convert format to that which is received over the wire in lambdas
+ */
+fun Event.asServerlessEvent() = EventBridgeEvent().apply {
+    detail = EventBridgeMoshi.asA<Map<String, Any>>(Detail.value)
+    detailType = DetailType.value
+    source = Source.value
+    resources = Resources?.map(ARN::value)
+}
