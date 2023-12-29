3d0c9aac713d6516c77e2a06bd6b850dd9149ce6
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-12-29 16:42:49 -0500

    S3 presigner (#340)
    * Add S3Presigner
    
    * use updated presigner
    
    * fix path-style request for s3 presigner

---
+++ amazon/s3/client/src/main/kotlin/org/http4k/connect/amazon/s3/model/S3BucketPreSigner.kt
@@ -0,0 +1,63 @@
+package org.http4k.connect.amazon.s3.model
+
+import org.http4k.aws.AwsCredentialScope
+import org.http4k.aws.AwsCredentials
+import org.http4k.aws.AwsRequestPreSigner
+import org.http4k.connect.amazon.CredentialsProvider
+import org.http4k.connect.amazon.core.model.Region
+import org.http4k.connect.amazon.s3.S3
+import org.http4k.core.Headers
+import org.http4k.core.Method.DELETE
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.PUT
+import org.http4k.core.Request
+import org.http4k.core.Uri
+import org.http4k.core.appendToPath
+import java.time.Clock
+import java.time.Duration
+
+class S3BucketPreSigner(
+    bucketName: BucketName,
+    region: Region,
+    credentialsProvider: CredentialsProvider,
+    clock: Clock = Clock.systemDefaultZone()
+) {
+    constructor(
+        bucketName: BucketName,
+        region: Region,
+        credentials: AwsCredentials,
+        clock: Clock = Clock.systemDefaultZone()
+    ) : this(
+        bucketName = bucketName,
+        region = region,
+        credentialsProvider = { credentials },
+        clock = clock
+    )
+
+    val preSigner = AwsRequestPreSigner(
+        credentialsProvider = credentialsProvider,
+        scope = AwsCredentialScope(region.value, S3.awsService.value),
+        clock = clock
+    )
+
+    private val bucketUri = let {
+        val pathPrefix = if (bucketName.requiresPathStyleApi()) "/$bucketName" else ""
+        val subdomain = if (bucketName.requiresPathStyleApi()) "" else "$bucketName."
+        Uri.of("https://$subdomain${S3.awsService}.$region.amazonaws.com").path(pathPrefix)
+    }
+
+    fun get(key: BucketKey, duration: Duration, headers: Headers = emptyList()) = preSigner(
+        Request(GET, bucketUri.appendToPath(key.value)).headers(headers),
+        duration
+    )
+
+    fun put(key: BucketKey, duration: Duration, headers: Headers = emptyList()) = preSigner(
+        Request(PUT, bucketUri.appendToPath(key.value)).headers(headers),
+        duration
+    )
+
+    fun delete(key: BucketKey, duration: Duration, headers: Headers = emptyList()) = preSigner(
+        Request(DELETE, bucketUri.appendToPath(key.value)).headers(headers),
+        duration
+    )
+}
