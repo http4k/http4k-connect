f1553cd85267608d4c0e6b146d7a10d99b57d667
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-04-23 17:21:37 -0400

    deflake RealSQSTest (#409)

--- amazon/sqs/client/src/test/kotlin/org/http4k/connect/amazon/sqs/RealSQSTest.kt
+++ amazon/sqs/client/src/test/kotlin/org/http4k/connect/amazon/sqs/RealSQSTest.kt
@@ -3,9 +3,11 @@ package org.http4k.connect.amazon.sqs
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
+import java.time.Duration
 
 class RealSQSTest : SQSContract(JavaHttpClient()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
+    override val retryTimeout: Duration = Duration.ofMinutes(1)
     override fun waitABit() {
         Thread.sleep(10000)
     }
