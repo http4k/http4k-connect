ade2a06a2f4fca94b3562a85a21c7e0ecb065eaf
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-04-23 14:45:47 -0400

    Properly parse evidently project and features names from ARN (#408)
    * Properly parse evidently project and features names from ARN
    
    * add missing files

--- amazon/evidently/client/src/main/kotlin/org/http4k/connect/amazon/evidently/model/ProjectName.kt
+++ amazon/evidently/client/src/main/kotlin/org/http4k/connect/amazon/evidently/model/ProjectName.kt
@@ -1,6 +1,6 @@
 package org.http4k.connect.amazon.evidently.model
 
-import dev.forkhandles.values.StringValueFactory
+import dev.forkhandles.values.ValueFactory
 import dev.forkhandles.values.and
 import dev.forkhandles.values.maxLength
 import dev.forkhandles.values.regex
@@ -8,7 +8,11 @@ import org.http4k.connect.amazon.core.model.ARN
 import org.http4k.connect.amazon.core.model.ResourceId
 
 class ProjectName private constructor(value: String) : ResourceId(value) {
-    companion object : StringValueFactory<ProjectName>(::ProjectName, 2048.maxLength.and("^[-a-zA-Z0-9._]*\$".regex)) {
-        fun of(arn: ARN) = arn.resourceId(::ProjectName)
+    companion object : ValueFactory<ProjectName, String>(
+        coerceFn = ::ProjectName,
+        validation = 2048.maxLength.and("^[-a-zA-Z0-9._]*\$".regex),
+        parseFn = { it.split("/").first() }
+    ) {
+        fun of(arn: ARN) = arn.resourceId(ProjectName::parse)
     }
 }
