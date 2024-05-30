2fd963147e13008214ba16049e2a8ed1f5f09752
David Denton <denton.david@gmail.com>
2024-05-30 12:13:22 +0100

    upgrades

--- amazon/evidently/client/src/test/kotlin/org/http4k/connect/amazon/evidently/RealEvidentlyTest.kt
+++ amazon/evidently/client/src/test/kotlin/org/http4k/connect/amazon/evidently/RealEvidentlyTest.kt
@@ -3,7 +3,8 @@ package org.http4k.connect.amazon.evidently
 import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.RealAwsEnvironment
 import org.http4k.connect.amazon.configAwsEnvironment
+import org.http4k.filter.debug
 
-class RealEvidentlyContract : EvidentlyContract(JavaHttpClient()), RealAwsEnvironment {
+class RealEvidentlyTest : EvidentlyContract(JavaHttpClient().debug()), RealAwsEnvironment {
     override val aws get() = configAwsEnvironment()
 }
