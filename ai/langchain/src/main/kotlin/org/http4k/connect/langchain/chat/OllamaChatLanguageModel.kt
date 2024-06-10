a8dd0dfabbfb13ef368a9366140b82f66894811d
David Denton <denton.david@gmail.com>
2024-06-10 16:54:17 +0100

    Add chat language model for ollama

---
+++ ai/langchain/src/main/kotlin/org/http4k/connect/langchain/chat/OllamaChatLanguageModel.kt
@@ -0,0 +1,64 @@
+package org.http4k.connect.langchain.chat
+
+import dev.forkhandles.result4k.map
+import dev.langchain4j.agent.tool.ToolSpecification
+import dev.langchain4j.data.message.AiMessage
+import dev.langchain4j.data.message.ChatMessage
+import dev.langchain4j.data.message.ContentType
+import dev.langchain4j.data.message.ImageContent
+import dev.langchain4j.data.message.TextContent
+import dev.langchain4j.data.message.UserMessage
+import dev.langchain4j.model.chat.ChatLanguageModel
+import dev.langchain4j.model.output.Response
+import org.http4k.connect.model.Base64Blob
+import org.http4k.connect.ollama.Message
+import org.http4k.connect.ollama.ModelName
+import org.http4k.connect.ollama.Ollama
+import org.http4k.connect.ollama.ResponseFormat
+import org.http4k.connect.ollama.Role
+import org.http4k.connect.ollama.action.ModelOptions
+import org.http4k.connect.ollama.chatCompletion
+import org.http4k.connect.orThrow
+
+fun OllamaChatLanguageModel(
+    ollama: Ollama,
+    model: ModelName,
+    stream: Boolean? = null,
+    format: ResponseFormat? = null,
+    keep_alive: String? = null,
+    options: ModelOptions? = null,
+) = object : ChatLanguageModel {
+    override fun generate(p0: List<ChatMessage>) = generate(p0, emptyList())
+
+    override fun generate(
+        messages: List<ChatMessage>,
+        toolSpecifications: List<ToolSpecification>
+    ): Response<AiMessage> {
+        if (toolSpecifications.isNotEmpty()) error("ToolSpecifications are not supported")
+
+        return Response(
+            ollama.chatCompletion(model, messages.map { it.toHttp4k() }, stream, format, keep_alive, options)
+                .map { AiMessage(it.mapNotNull { it.message?.content }.joinToString("")) }
+                .orThrow()
+        )
+    }
+}
+
+private fun ChatMessage.toHttp4k(): Message = when (this) {
+    is UserMessage -> toHttp4k()
+    is AiMessage -> toHttp4k()
+    else -> error("unknown message type")
+}
+
+private fun UserMessage.toHttp4k(): Message {
+    val text = contents().filter { it.type() == ContentType.TEXT }
+        .takeIf { it.size == 1 }
+        ?.first()?.let { (it as TextContent).text() }
+        ?: error("Only single text is supported")
+
+    val images = contents().filter { it.type() == ContentType.IMAGE }
+        .map { Base64Blob.of((it as ImageContent).image().base64Data()) }
+    return Message(Role.user, text, images)
+}
+
+private fun AiMessage.toHttp4k() = Message(Role.assistant, text())
