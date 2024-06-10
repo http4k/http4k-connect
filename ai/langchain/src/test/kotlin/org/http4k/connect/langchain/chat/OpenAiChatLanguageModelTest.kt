4ff8786ceaf8d4854ba4bcd837cb44f16b1eac18
David Denton <denton.david@gmail.com>
2024-06-10 16:32:21 +0100

    rely on fakes to test models

--- ai/langchain/src/test/kotlin/org/http4k/connect/langchain/chat/OpenAiChatLanguageModelTest.kt
+++ ai/langchain/src/test/kotlin/org/http4k/connect/langchain/chat/OpenAiChatLanguageModelTest.kt
@@ -5,7 +5,7 @@ import org.http4k.connect.openai.Http
 import org.http4k.connect.openai.OpenAI
 import org.http4k.connect.openai.OpenAIToken
 
-class FakeOpenAiChatLanguageModelTest : ChatLanguageModelContract {
+class OpenAiChatLanguageModelTest : ChatLanguageModelContract {
     override val model by lazy {
         OpenAiChatLanguageModel(
             OpenAI.Http(OpenAIToken.of("hello"), FakeOpenAI()),
