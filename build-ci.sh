75c3f2ac14da84c2315be2a658f954d47e11bac9
David Denton <denton.david@gmail.com>
2024-06-26 17:48:25 +0200

    Revert "turn on cache and gradle info loggin"
    This reverts commit 28e4a935667bc0bbdf2291c2b0eaf49d77701eb7.

--- build-ci.sh
+++ build-ci.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew check jacocoRootReport --build-cache --parallel --info
+./gradlew check jacocoRootReport --build-cache --parallel
 bash <(curl -s https://codecov.io/bash)
