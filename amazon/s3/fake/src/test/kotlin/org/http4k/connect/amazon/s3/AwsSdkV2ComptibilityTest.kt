333312f3b01a1d790e387c46ef2ad659636aa20c
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-06-27 09:42:22 -0400

    fix AwsSdkS3 compatibility test

--- amazon/s3/fake/src/test/kotlin/org/http4k/connect/amazon/s3/AwsSdkV2ComptibilityTest.kt
+++ amazon/s3/fake/src/test/kotlin/org/http4k/connect/amazon/s3/AwsSdkV2ComptibilityTest.kt
@@ -2,8 +2,6 @@ package org.http4k.connect.amazon.s3
 
 import org.http4k.aws.AwsSdkClient
 import org.http4k.connect.amazon.configAwsEnvironment
-import org.http4k.core.then
-import org.http4k.filter.RequestFilters
 import org.junit.jupiter.api.Test
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.core.sync.RequestBody
@@ -11,48 +9,44 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.StorageClass
 import software.amazon.awssdk.services.s3.model.Tier
-
-private val BUCKET_NAME = "aohara-test"
+import java.util.*
 
 class AwsSdkV2ComptibilityTest {
 
     private val fake = FakeS3()
 
+    private val bucket = UUID.randomUUID().toString()
     private val key = "bar.txt"
 
-    private val filter = RequestFilters.Tap {
-        println(it)
-    }
-
     private val client = S3Client.builder()
+        .httpClient(AwsSdkClient(fake))
         .credentialsProvider {
             val creds = configAwsEnvironment().credentials
             AwsBasicCredentials.create(creds.accessKey, creds.secretKey)
         }
         .region(Region.US_EAST_1)
-
-//        .httpClient(AwsSdkClient(filter.then(fake)))
-//        .credentialsProvider { AwsBasicCredentials.create("id", "secret") }
-//        .region(Region.CA_CENTRAL_1)
         .build()
 
     @Test
     fun restore() {
+        client.createBucket {
+            it.bucket(bucket)
+        }
+
         client.putObject({
-            it.bucket(BUCKET_NAME)
+            it.bucket(bucket)
             it.key(key)
             it.storageClass(StorageClass.GLACIER)
         }, RequestBody.fromString("foo"))
 
         client.restoreObject {
-            it.bucket(BUCKET_NAME)
+            it.bucket(bucket)
             it.key(key)
             it.restoreRequest { req ->
                 req.days(2)
                 req.glacierJobParameters {  param ->
                     param.tier(Tier.EXPEDITED)
                 }
-//                req.tier(Tier.EXPEDITED)
             }
         }
     }
