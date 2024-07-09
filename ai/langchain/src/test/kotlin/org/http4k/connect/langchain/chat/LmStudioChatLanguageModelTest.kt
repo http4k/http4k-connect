05a76f99226a0ce4f7afdc04a162ce654592b924
David Denton <denton.david@gmail.com>
2024-07-09 18:10:42 +0100

    Extracted out "ignore lmstudio if not running" test

--- ai/langchain/src/test/kotlin/org/http4k/connect/langchain/chat/LmStudioChatLanguageModelTest.kt
+++ ai/langchain/src/test/kotlin/org/http4k/connect/langchain/chat/LmStudioChatLanguageModelTest.kt
@@ -1,12 +1,13 @@
 package org.http4k.connect.langchain.chat
 
+import org.http4k.connect.langchain.LmStudioTestCase
 import org.http4k.connect.lmstudio.CHAT_MODEL
 import org.http4k.connect.lmstudio.FakeLmStudio
 import org.http4k.connect.lmstudio.Http
 import org.http4k.connect.lmstudio.LmStudio
 import org.http4k.connect.model.ModelName
 
-class LmStudioChatLanguageModelTest : ChatLanguageModelContract {
+class LmStudioChatLanguageModelTest : LmStudioTestCase(), ChatLanguageModelContract {
     override val model by lazy {
         LmStudioChatLanguageModel(
             LmStudio.Http(FakeLmStudio()),
