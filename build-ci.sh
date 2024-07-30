6ba24daefb40fafb286c8cd50c075100a598dd2f
David Denton <denton.david@gmail.com>
2024-07-30 12:42:58 +0100

    Add further logging level to build

--- build-ci.sh
+++ build-ci.sh
@@ -5,6 +5,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew check --info
+./gradlew check --debug
 #./gradlew check --build-cache --parallel
 #bash <(curl -s https://codecov.io/bash)
