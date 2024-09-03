1787cac08f48976120bc84baecc874011a2fc93f
David Denton <denton.david@gmail.com>
2024-09-03 16:11:10 +0100

    Add default environment to CredentialsProvider

--- amazon/core/src/main/kotlin/org/http4k/connect/amazon/CredentialsProvider.kt
+++ amazon/core/src/main/kotlin/org/http4k/connect/amazon/CredentialsProvider.kt
@@ -2,12 +2,13 @@ package org.http4k.connect.amazon
 
 import org.http4k.aws.AwsCredentials
 import org.http4k.config.Environment
+import org.http4k.config.Environment.Companion.ENV
 
 fun interface CredentialsProvider : () -> AwsCredentials {
     companion object
 }
 
-fun CredentialsProvider.Companion.Environment(env: Environment) = CredentialsProvider { AWS_CREDENTIALS(env) }
+fun CredentialsProvider.Companion.Environment(env: Environment = ENV) = CredentialsProvider { AWS_CREDENTIALS(env) }
 
 fun CredentialsProvider.Companion.Environment(env: Map<String, String> = System.getenv()) =
     Environment(Environment.from(env))
