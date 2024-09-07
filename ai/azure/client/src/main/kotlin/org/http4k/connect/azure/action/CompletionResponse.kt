9a3ce61cf9e19822652a03bef0c525cf1726e5c9
David Denton <denton.david@gmail.com>
2024-09-07 19:56:55 +0100

    Remove dead file

--- ai/azure/client/src/main/kotlin/org/http4k/connect/azure/action/CompletionResponse.kt
+++
@@ -1,8 +0,0 @@
-package org.http4k.connect.azure.action
-
-import org.http4k.connect.azure.CompletionId
-import org.http4k.connect.azure.ObjectType
-import org.http4k.connect.model.ModelName
-import org.http4k.connect.model.Timestamp
-import se.ansman.kotshi.JsonProperty
-import se.ansman.kotshi.JsonSerializable
